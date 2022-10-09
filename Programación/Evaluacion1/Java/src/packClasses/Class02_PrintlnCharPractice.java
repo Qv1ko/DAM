1ee0cdf9bd87a7cf73a24f4034a076c83d09fca8
Qv1ko <vicgarmur947@gmail.com>
2022-10-09 23:07:48 +0200

    Create folders and java class

---
+++ Programación/Evaluacion1/Java/src/packClasses/Class02_PrintlnCharPractice.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Class02_PrintlnCharPractice	{
+
+    public static void main(String args[])	{
+    	
+    	// println -> muestra por consola el caracter entre comillas y ejecuta un salto de linea a continuacion del texto
+    	System.out.println('*');
+
+    }
+    
+}
