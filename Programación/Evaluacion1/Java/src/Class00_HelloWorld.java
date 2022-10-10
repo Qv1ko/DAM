e60fe2c4ddaf39061d8249c58971596d85d65dca
Qv1ko <vicgarmur947@gmail.com>
2022-10-10 18:11:23 +0200

    Java tree

---
+++ Programación/Evaluacion1/Java/src/Class00_HelloWorld.java
@@ -0,0 +1,19 @@
+// Las palabras con color distintivo son palabras reservadas de java
+
+// Se selecciona el paquete en el que se encuentra la clase/fichero .java
+//package src;
+
+/* Cada clase, pertenece a un fichero .java
+ * Para que una clase pueda ser usada en el codigo tiene que ser de tipo Public
+ */
+public class Class00_HelloWorld	{
+
+	// main -> metodo que permite ejecutar el codigo
+    public static void main(String args[])	{
+    	
+    	// println -> muestra por consola el texto entre doble comilla y ejecuta un salto de linea a continuacion del texto
+    	System.out.println("Hello world!!");
+
+    }
+    
+}
