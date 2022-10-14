f52ea640fbeb8e7cd5befb92d7fa11dec0c075c9
Qv1ko <vicgarmur947@gmail.com>
2022-10-14 13:08:50 +0200

    Data base class

--- Bases de datos/Evaluacion1/Exercise00_Prestamos.sql
+++ Bases de datos/Evaluacion1/Exercise00_Prestamos.sql
@@ -1,5 +1,5 @@
-create database Exercise00;
+create database exercise00;
 
-use Exercise00;
+use exercise00;
 
 create table Prestamos(Numero_prestamo int primary key, Codigo_socio int);
---
+++ Bases de datos/Evaluacion1/Exercise01_Socios.sql
@@ -0,0 +1,5 @@
+create database exercise01;
+
+use exercise01;
+
+create table Socios(Numero_socio int primary key, Apellidos varchar(15), Telefono char(9), Direccion varchar(20), Codigo_postal int);
