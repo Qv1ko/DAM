7bc855f6c08cc98262cbeb27e281f381e6d3fa70
Qv1ko <vicgarmur947@gmail.com>
2022-10-14 10:58:24 +0200

    While and Do While class

--- Programación/Evaluacion1/Java/src/packClasses/Class07_BucleWhile.java
+++ Programación/Evaluacion1/Java/src/packClasses/Class07_BucleWhile.java
@@ -13,7 +13,7 @@ public class Class07_BucleWhile {
             contador++;
 
         }
-        System.out.println("\n");
+        System.out.print("\n");
 
         //ejercicio 0 - Tabla multiplicar
         int cont=0,tabla=3;
@@ -24,7 +24,7 @@ public class Class07_BucleWhile {
             cont++;
 
         }
-        System.out.println("\n");
+        System.out.print("\n");
 
         //ejercicio 1 - Numeros pares de 100 a 0
         int num=100;
@@ -38,6 +38,39 @@ public class Class07_BucleWhile {
             
             System.out.println(num);
             num-=2;
+
+        }
+        System.out.print("\n");
+
+        //ejemplo 1 - Numeros pares de 100 a 48
+        int number=100;
+
+        while(number>=0) {
+
+            System.out.println(number);
+            number-=2;
+
+            //rompe el bucle al cumplirse la condicion
+            if(number==46) {
+                break;
+            }
+
+        }
+        System.out.print("\n");
+
+        //ejemplo 2 - Numeros pares de 100 a 0 menos el 48
+        int num1=100;
+
+        while(num1>=0) {
+
+            //continua el bucle al cumplirse la condicion sin mostrar el valor de la variable
+            if(num1==48) {
+                num1-=2;
+                continue;
+            }
+
+            System.out.println(num1);
+            num1-=2;      
 
         }
 
