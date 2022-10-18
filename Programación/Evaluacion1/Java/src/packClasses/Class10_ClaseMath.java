3a0c480366490ccfc585dddab884f5f137a7da92
Qv1ko <vicgarmur947@gmail.com>
2022-10-18 10:14:17 +0200

    New class Math and correction Exercise03_Repaso1Weekend.

---
+++ Programación/Evaluacion1/Java/src/packClasses/Class10_ClaseMath.java
@@ -0,0 +1,13 @@
+package packClasses;
+
+public class Class10_ClaseMath {
+    
+    public static void main(String[] args) {
+        
+
+        System.out.println(Math.pow(2, 3)); //Potencia de 2^3
+        System.out.println(Math.random()); //numero aleatorio entre 0.0 y 1.0
+
+    }
+
+}
--- Programación/Evaluacion1/Java/src/packExercises/Exercise03_Repaso1Weekend.java
+++ Programación/Evaluacion1/Java/src/packExercises/Exercise03_Repaso1Weekend.java
@@ -7,15 +7,16 @@ public class Exercise03_Repaso1Weekend {
         int planta1=200,planta2=150,planta3=100; //plazas disponibles en cada planta
         int pesoVehiculo=300; //peso del vehiculo
 
-        if(pesoVehiculo<500&&planta1>=planta2) {
+        // pesoVehiculo=(int)Math.random(); //peso del vehiculo aleatorio
+        // pesoVehiculo=(int)(Math.random()*1000); //peso del vehiculo por mil
+        // pesoVehiculo=(int)(Math.random()*700)+300; //peso del vehiculo minimo 300
+
+        if(pesoVehiculo<500&&planta1>0) {
             System.out.println("Su vehiculo de "+pesoVehiculo+"Kg debe aparcar en la primera planta");
-            planta1--;
-        } else if(pesoVehiculo<500&&planta1<planta2) {
+        } else if(pesoVehiculo<500&&planta2>0) {
             System.out.println("Su vehiculo de "+pesoVehiculo+"Kg debe aparcar en la segunda planta");
-            planta2--;
-        } else if(planta3>0) {
+        } else if(pesoVehiculo>=500&&planta3>0) {
             System.out.println("Su vehiculo de "+pesoVehiculo+"Kg debe aparcar en la tercera planta");
-            planta3--;
         } else {
             System.out.println("No hay plazas para aparcar");
         }
