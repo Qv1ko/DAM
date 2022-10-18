d895284223dc92b94cd87cd58cd7b3508903d2ba
Qv1ko <vicgarmur947@gmail.com>
2022-10-18 09:23:32 +0200

    Comments.

--- Programación/Evaluacion1/Java/src/packExercises/Exercise03_Repaso1Weekend.java
+++ Programación/Evaluacion1/Java/src/packExercises/Exercise03_Repaso1Weekend.java
@@ -4,24 +4,24 @@ public class Exercise03_Repaso1Weekend {
 
     public static void main(String[] args) {
         
-        int planta1=200,planta2=150,planta3=100;
-        int pesoVehiculo=200;
+        int planta1=200,planta2=150,planta3=100; //plazas disponibles en cada planta
+        int pesoVehiculo=300; //peso del vehiculo
 
         if(pesoVehiculo<500&&planta1>=planta2) {
 
-            System.out.println("Debe aparcar en la primera planta");
+            System.out.println("Su vehiculo de "+pesoVehiculo+"Kg debe aparcar en la primera planta");
             planta1--;
 
         }
         else if(pesoVehiculo<500&&planta1<planta2) {
 
-            System.out.println("Debe aparcar en la segunda planta");
+            System.out.println("Su vehiculo de "+pesoVehiculo+"Kg debe aparcar en la segunda planta");
             planta2--;
 
         }
         else if(planta3>0) {
 
-            System.out.println("Debe aparcar en la tercera planta");
+            System.out.println("Su vehiculo de "+pesoVehiculo+"Kg debe aparcar en la tercera planta");
             planta3--;
 
         }
