3228b79bf8165362f57fd31093ecb665f068ff7b
Qv1ko <vicgarmur947@gmail.com>
2022-10-23 13:59:58 +0200

    Delete \n.

--- Programación/Evaluacion1/Java/src/packExercises/Exercise05_Factorial.java
+++ Programación/Evaluacion1/Java/src/packExercises/Exercise05_Factorial.java
@@ -4,13 +4,13 @@ public class Exercise05_Factorial {
 
     public static void main(String[] args) {
         
-        long numY=17, Resultado=1;
+        long numY=7, Resultado=1;
 
         while(numY>1) {
             Resultado*=numY;
             numY--;
         }
-        System.out.println("\nEl resultado es "+Resultado);        
+        System.out.println("El resultado es "+Resultado);        
 
     }//fin main
     
