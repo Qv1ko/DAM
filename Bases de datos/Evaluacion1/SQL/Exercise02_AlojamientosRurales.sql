316162e9bc6a5228c15228cb802d379a139a41e6
Qv1ko <vicgarmur947@gmail.com>
2022-10-24 15:42:32 +0200

    End of exercise02.

--- Bases de datos/Evaluacion1/SQL/Exercise02_AlojamientosRurales.sql
+++ Bases de datos/Evaluacion1/SQL/Exercise02_AlojamientosRurales.sql
@@ -2,14 +2,43 @@ CREATE DATABASE Exercise02_AlojamientosRurales;
 
 USE Exercise02_AlojamientosRurales;
 
-CREATE TABLE actividad(
-    codigo INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE organizacion(
     telefono CHAR(9),
-    nombre VARCHAR(25)
+    nombre VARCHAR(20),
+    web VARCHAR(100) NOT NULL,
+    PRIMARY KEY(nombre)
 );
 
-CREATE TABLE espacioNatural(
-    nombre CHAR(50) PRIMARY KEY,
-    extension INT NULL,
-    epoca 
+CREATE TABLE espacio_natural(
+    extension FLOAT(5,3),
+    nombre VARCHAR(35),
+    epoca CHAR(9) CHECK(epoca="otono"OR"invierno"OR"verano"OR"primavera"),
+    PRIMARY KEY(nombre)
+);
+
+CREATE TABLE alojamiento(
+    nombre VARCHAR(20) PRIMARY KEY,
+    direccion VARCHAR(30) UNIQUE,
+    telefono CHAR(9),
+    comunidad_autonoma VARCHAR(25),
+    tipo_alquiler VARCHAR(20) CHECK(tipo_alquiler="completo"OR"por habitaciones"),
+    organizacion_nombre VARCHAR(20),
+    espacio_natural_nombre VARCHAR(35),
+    CONSTRAINT fk_es FOREIGN KEY(espacio_natural_nombre) REFERENCES espacio_natural(nombre),
+    CONSTRAINT fk_org FOREIGN KEY(organizacion_nombre) REFERENCES organizacion(nombre)
+);
+
+CREATE TABLE actividad(
+    codigo VARCHAR(20) PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    telefono CHAR(9) NOT NULL
+);
+
+CREATE TABLE ofertar(
+    precio FLOAT(5,2) NOT NULL,
+    dificultad VARCHAR(15) CHECK(dificultad="facil"OR"medio"OR"dificil"OR"experto"),
+    espacio_natural_nombre VARCHAR(35),
+    actividad_codigo VARCHAR(20),
+    CONSTRAINT fk_es2 FOREIGN KEY(espacio_natural_nombre) REFERENCES espacio_natural(nombre),
+    CONSTRAINT fk_act FOREIGN KEY(actividad_codigo) REFERENCES actividad(codigo)
 );
