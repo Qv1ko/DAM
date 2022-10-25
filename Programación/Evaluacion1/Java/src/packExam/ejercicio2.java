257602d9f359531d07b9040edcb8f7d36b2a0a32
Qv1ko <vicgarmur947@gmail.com>
2022-10-25 17:22:08 +0200

    Examen1 - Evaluacion1.

---
+++ Programación/Evaluacion1/Java/src/packExam/ejercicio2.java
@@ -0,0 +1,16 @@
+package packExam;
+
+public class ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        double gradosKel=0,gradosFah=0;
+
+        for(double i=gradosKel;i<=500;i+=50) {
+            gradosFah=((((i-273.15)*9)/5)+32);
+            System.out.println(i+" grados Kelvin es igual a "+gradosFah+" grados Fahrenheit");
+        }
+
+    } //fin main
+    
+} //fin class
