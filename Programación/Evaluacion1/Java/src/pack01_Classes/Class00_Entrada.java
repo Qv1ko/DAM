d74c701763f23fc7543cc74cf2d248e74dab41d8
Qv1ko <vicgarmur947@gmail.com>
2022-10-28 11:00:08 +0200

    Scanner class

---
+++ Programación/Evaluacion1/Java/src/pack01_Classes/Class00_Entrada.java
@@ -0,0 +1,30 @@
+package pack01_Classes;
+
+import java.util.Scanner;
+
+public class Class00_Entrada {
+
+    public static void main(String[] args) {
+        
+        //String nombre="Victor"; //referencia a un objeto (nombre es un objeto)
+        
+        //declaracion de un objeto (porque el tipo de dato que precede al identificador esta en mayus -> Scanner)
+        //todo objeto en java tiene un construcctor
+        Scanner entrada; //declaramos e instanciamos(guardar un tamano en memoria para el objeto) el objeto
+
+        //declaracion de variable
+        long numero;
+
+        entrada=new Scanner(System.in); //definimos como parametro de entrada para el Scanner la clase Input System
+
+        System.out.println("Introduce un numero");
+        numero=entrada.nextLong(); //llamada de un metodo de la clase Scanner para introducir el metodo por teclado al ejecutar
+        System.out.println("Has introducido -> "+numero);
+
+        System.out.println("Introduce tu nombre");
+        String cadena=entrada.next(); //tambien se puede usar nextLine()
+        System.out.println("Tu nombre es -> "+cadena);
+        
+    } //fin main
+
+} //fin class
