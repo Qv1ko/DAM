4f0ea40e3412da2a24ecfd4f8abe397193b8d26b
Qv1ko <vicgarmur947@gmail.com>
2022-10-30 22:36:20 +0100

    started exercise 3.

---
+++ Programación/Evaluacion1/Java/src/pack01_Exercises/Exercise00_EjerciciosScanner3.java
@@ -0,0 +1,20 @@
+package pack01_Exercises;
+
+import java.util.Scanner;
+
+public class Exercise00_EjerciciosScanner3 {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada=new Scanner(System.in);
+
+        System.out.print("Escribe un numero: ");
+        int num1=entrada.nextInt();
+        System.out.print("Escribe un numero: ");
+        int num2=entrada.nextInt();
+
+        
+
+    }
+    
+}
