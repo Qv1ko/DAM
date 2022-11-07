c59831f1d159da32cbc1620fc0411a3f1a4a5287
Qv1ko <vicgarmur947@gmail.com>
2022-11-07 13:52:49 +0100

    Exercise00_EjerciciosScanner1 correction.

--- Programación/Evaluacion1/Java/src/pack01_Exercises/Exercise00_EjerciciosScanner1.java
+++ Programación/Evaluacion1/Java/src/pack01_Exercises/Exercise00_EjerciciosScanner1.java
@@ -7,16 +7,16 @@ public class Exercise00_EjerciciosScanner1 {
     public static void main(String[] args) {
         
         Scanner entrada=new Scanner(System.in); //declaracion e instanciacion del input con Scanner
-        int ventas=0,productos=0,suma=0;
+        int ventas=0;
+        float total=0.0f;
 
         System.out.print("Escriba el numero de ventas: ");
         ventas=entrada.nextInt();
         for(int i=ventas;i>0;i--) {
-            System.out.print("Escriba el numero de productos comprados: ");
-            productos=entrada.nextInt();
-            suma+=productos;
+            System.out.print("Escriba el importe de la "+i+"venta : ");
+            total+=entrada.nextFloat();
         }
-        System.out.println("Total de productos vendidos -> "+suma);
+        System.out.println("Total de productos vendidos -> "+total);
 
         entrada.close();
 
