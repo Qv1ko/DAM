85768bbc103478750956ff4811623c77c1f3ef2e
Qv1ko <vicgarmur947@gmail.com>
2022-11-09 11:02:35 +0100

    Exercise00_EjerciciosScanner5 correction.

--- Programación/Evaluacion1/Java/src/pack01_Exercises/Exercise00_EjerciciosScanner5.java
+++ Programación/Evaluacion1/Java/src/pack01_Exercises/Exercise00_EjerciciosScanner5.java
@@ -7,16 +7,14 @@ public class Exercise00_EjerciciosScanner5 {
     public static void main(String[] args) {
         
         Scanner entrada=new Scanner(System.in);
-        int num=0,mayor=0,menor=0,sumTotal=0,sumPos=0,sumNeg=0,count=0;
+        int num=0;
+        int mayor=0,menor=Integer.MAX_VALUE /*valor maximo de un entero, con la clase Integer */,sumTotal=0,sumPos=0,sumNeg=0,count=0;
         float mediaSum=0;
 
-        while(true) {
+        while(num!=-1) {
             System.out.print("Escriba un numero: ");
             num=entrada.nextInt();
-
-            if(num==-1) {
-                break;
-            } else {
+            if(num!=-1) {
                 if(num>mayor) {
                     mayor=num;
                 }
@@ -35,6 +33,31 @@ public class Exercise00_EjerciciosScanner5 {
                 mediaSum=(float)sumTotal/count;
             }
         }
+
+        // do {
+        //     System.out.print("Escriba un numero: ");
+        //     num=entrada.nextInt();
+
+        //     if (num!=-1) {
+        //         if(num>mayor) {
+        //             mayor=num;
+        //         }
+        //         if(num<menor) {
+        //             menor=num;
+        //         }
+        //         if(num>=0) {
+        //             sumTotal+=num;
+        //             count++;
+        //             sumPos+=num;
+        //         } else {
+        //             sumTotal+=num;
+        //             count++;
+        //             sumNeg+=num;
+        //         }
+        //         mediaSum=(float)sumTotal/count;
+        //     }
+        // } while(num!=-1);
+
         System.out.println("\nMayor numero introducido -> "+mayor+"\nMenor numero introducido -> "+menor);
         System.out.println("Suma de todos los numeros -> "+sumTotal+"\nSuma de los numeros positivos -> "+sumPos+"\nSuma de los numeros negativos -> "+sumNeg);
         System.out.println("Media de la suma total -> "+mediaSum);
