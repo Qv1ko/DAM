ed3081f74d5eae1cea43ede00103d3fa10b3a72d
Qv1ko <vicgarmur947@gmail.com>
2022-11-18 10:59:59 +0100

    Programming class

---
+++ Programación/Evaluacion1/Java/src/pack01_Classes/Class04_ComaPunto.java
@@ -0,0 +1,25 @@
+package pack01_Classes;
+
+import java.util.Scanner;
+
+public class Class04_ComaPunto {
+
+    public static void main(String[] args) {
+        
+        Scanner ent=new Scanner(System.in);
+
+        //ejemplo 0 - Numero con coma
+        System.out.print("Escribe un numero decimal con coma: ");
+        float numeroComa=ent.nextFloat();
+        System.out.println("Mi numero es -> "+numeroComa);
+
+        //ejemplo 1 - Numero con punto
+        System.out.print("Escribe un numero decimal con punto: ");
+        float numeroPunto=Float.parseFloat(ent.next().replace(',', '.'));
+        System.out.println("Mi numero es -> "+numeroPunto);
+
+        ent.close();
+
+    } //fin main
+    
+} //fin class
