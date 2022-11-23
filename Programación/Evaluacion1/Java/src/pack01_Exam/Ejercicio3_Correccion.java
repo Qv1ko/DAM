1867d0198df0ff87af5794a339c245dc8ed7a49e
Qv1ko <vicgarmur947@gmail.com>
2022-11-23 10:15:22 +0100

    Exam exercises correction

---
+++ Programación/Evaluacion1/Java/src/pack01_Exam/Ejercicio3_Correccion.java
@@ -0,0 +1,17 @@
+package pack01_Exam;
+
+import java.util.Scanner;
+
+public class Ejercicio3_Correccion {
+
+    public static void main(String[] args) {
+        
+        Scanner ent=new Scanner(System.in);
+
+        System.out.print("Introduce la carta: ");
+
+        ent.close();
+
+    } //fin main
+    
+} //fin class
