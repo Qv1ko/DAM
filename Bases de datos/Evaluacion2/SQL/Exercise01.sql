071532ebb5378b6cf505aed5e61203f82e8e6204
Qv1ko <vicgarmur947@gmail.com>
2022-11-24 13:56:05 +0100

    Database exercise01

---
+++ Bases de datos/Evaluacion2/SQL/Exercise01.sql
@@ -0,0 +1,9 @@
+SELECT * FROM automoviles;
+
+SELECT * FROM clientes;
+
+SELECT * FROM contratos;
+
+SELECT apellidos,nombre FROM clientes;
+
+SELECT apellidos,nombre FROM clientes ORDER BY apellidos ASC;
