9e5a73d2f8dde4e5bfde6929a427623703428aa1
Qv1ko <vicgarmur947@gmail.com>
2022-12-14 10:16:31 +0100

    Resolucion del Ejercicio03_ArraysPares

--- Programación/Java/src/paqueteEjercicios2/Ejercicio03_ArraysPares.java
+++ Programación/Java/src/paqueteEjercicios2/Ejercicio03_ArraysPares.java
@@ -5,19 +5,39 @@ import java.util.Arrays;
 public class Ejercicio03_ArraysPares {
 
     public static void main(String[] args) {
-        int[] numeros=new int[15],numPares=new int[15],numImpares=new int[15];
-        int contPares=0,contImpares=0;
-        for(int i=0;i<numeros.length;i++) {
-            numeros[i]=(int)(Math.random()*101);
-            if(numeros[i]%2==0) {
-                numPares[contPares]=numeros[i];
-                contPares++;
+        // int[] numeros=new int[15],numPares,numImpares;
+        // int par=0,contPar=0,contImpar=0;
+        // for(int i=0;i<numeros.length;i++) {
+        //     numeros[i]=(int)(Math.random()*101);
+        //     if(numeros[i]%2==0) {
+        //         par++;
+        //     }
+        // }
+        // numPares=new int[par];
+        // numImpares=new int[numeros.length-par];
+        // for(int i:numeros) {
+        //     if(i%2==0) {
+        //         numPares[contPar]=i;
+        //         contPar++;
+        //     } else {
+        //         numImpares[contImpar]=i;
+        //         contImpar++;
+        //     }
+        // }
+        // System.out.println("Numeros pares -> "+Arrays.toString(numPares)+"\nNumeros impares -> "+Arrays.toString(numImpares));
+    
+        int[] lista=new int[15],listaPares=new int[0],listaImpares=new int[0];
+        for(int i=0;i<lista.length;i++) {
+            lista[i]=(int)(Math.random()*101);
+            if(lista[i]%2==0) {
+                listaPares=Arrays.copyOf(listaPares, listaPares.length+1);
+                listaPares[listaPares.length-1]=lista[i];
             } else {
-                numImpares[contImpares]=numeros[i];
-                contImpares++;
+                listaImpares=Arrays.copyOf(listaImpares, listaImpares.length+1);
+                listaImpares[listaImpares.length-1]=lista[i];
             }
         }
-        System.out.println("Numeros pares -> "+Arrays.toString(numPares)+"\nNumeros impares -> "+Arrays.toString(numImpares));
-    }
+        System.out.println("Numeros pares -> "+Arrays.toString(listaPares)+"\nNumeros impares -> "+Arrays.toString(listaImpares));
+    }//main
 
-}
+}//class
