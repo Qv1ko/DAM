d713b59a926a0a1588e5b12cdce27180e3952132
Qv1ko <vicgarmur947@gmail.com>
2023-01-22 02:36:39 +0100

    Tarea programación finalizada

--- Programación/Java/src/paqueteEjercicios2/Ejercicio07_MiBanco.java
+++ Programación/Java/src/paqueteEjercicios2/Ejercicio07_MiBanco.java
@@ -17,11 +17,13 @@ public class Ejercicio07_MiBanco {
     private static void menu(Ejercicio06_CuentaCorriente[] clientes) {
         Scanner sc=new Scanner(System.in);
         int opcion=0;
-        String titular="";
+        String titular="",beneficiario="";
+        double cantidadIngresar=0.0,cantidadRetirar=0.0,cantidadTransferente=0.0;
+        boolean finDeTransferencia=false;
         do {
             do {
                 System.out.print("Opciones -> (1) Ver clientes | (2) Ingresar dinero | (3) Retirar dinero | (4) Transferir dinero | (5) Salir\nSeleccione una opcion: ");
-                opcion=sc.nextInt();
+                opcion=sc.nextInt();sc.nextLine();
             } while(opcion<1||opcion>5);
             switch(opcion) {
                 case 1: 
@@ -33,12 +35,48 @@ public class Ejercicio07_MiBanco {
                     System.out.print("Escriba el titular: ");
                     titular=sc.nextLine();
                     for(int i=0;i<clientes.length;i++) {
-                        if(clientes[i].getTitular()==titular) {
+                        if(clientes[i].getTitular().equalsIgnoreCase(titular)) {
                            System.out.print("Cantidad a ingresar: ");
-                           clientes[i].ingresar(sc.nextDouble());
+                           cantidadIngresar=sc.nextDouble();
+                           clientes[i].ingresar(cantidadIngresar);
+                           break;
                         }
                     }
-            }
+                    break;
+                case 3:
+                    System.out.print("Escriba el titular: ");
+                    titular=sc.nextLine();
+                    for(Ejercicio06_CuentaCorriente cliente:clientes) {
+                        if(cliente.getTitular().equalsIgnoreCase(titular)) {
+                            System.out.print("Cantidad a retirar: ");
+                            cantidadRetirar=sc.nextDouble();
+                            cliente.retirar(cantidadRetirar);
+                            break;
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.print("Titular ordenante: ");
+                    titular=sc.nextLine();
+                    System.out.print("Titular beneficiario: ");
+                    beneficiario=sc.nextLine();
+                    System.out.print("Cantidad a transferir: ");
+                    cantidadTransferente=sc.nextDouble();
+                    for(int i=0;i<clientes.length;i++) {
+                        for(Ejercicio06_CuentaCorriente cliente:clientes) {
+                            if(clientes[i].getTitular().equalsIgnoreCase(titular)&&cliente.getTitular().equalsIgnoreCase(beneficiario)) {
+                                clientes[i].retirar(cantidadTransferente);
+                                cliente.ingresar(cantidadTransferente);
+                                finDeTransferencia=true;
+                                break;
+                            }
+                        }
+                        if(finDeTransferencia==true) {break;}
+                    }
+                    break;
+                case 5:
+                    System.out.println("\nSaliendo...");
+            }//switch
         }while(opcion!=5);
         sc.close();
     }//menu
