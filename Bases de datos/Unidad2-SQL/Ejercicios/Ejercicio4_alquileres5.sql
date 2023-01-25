932a06b52028e9109366d8925a37f22bb1ee8ca3
Qv1ko <vicgarmur947@gmail.com>
2023-01-25 12:30:06 +0100

    Ejercicio 5 finalizado

--- Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres5.sql
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres5.sql
@@ -86,32 +86,11 @@ ALTER TABLE contratos ADD FOREIGN KEY(matricula_automoviles) REFERENCES automovi
 UPDATE automoviles
     SET matricula="4783CBJ" WHERE marca_marcas="Audi" AND modelo="A3";
 
--- UPDATE automoviles NATURAL JOIN contratos
---     SET kilometros=kfinal WHERE numcontrato=15;
-UPDATE automoviles SET kilometros=22561 WHERE matricula="1234BMY";
-UPDATE automoviles SET kilometros=NULL WHERE matricula="123BMR";
-UPDATE automoviles SET kilometros=9500 WHERE matricula="1678CCN";
-UPDATE automoviles SET kilometros=2500 WHERE matricula="1732CBS";
-UPDATE automoviles SET kilometros=45876 WHERE matricula="1978BNT";
-UPDATE automoviles SET kilometros=9736 WHERE matricula="2058CGF";
-UPDATE automoviles SET kilometros=34323 WHERE matricula="2123BTB";
-UPDATE automoviles SET kilometros=1120 WHERE matricula="2300CFG";
-UPDATE automoviles SET kilometros=1000 WHERE matricula="2301CFG";
-UPDATE automoviles SET kilometros=17368 WHERE matricula="3273BGH";
-UPDATE automoviles SET kilometros=7683 WHERE matricula="3765BSD";
-UPDATE automoviles SET kilometros=23057 WHERE matricula="4387BDD";
-UPDATE automoviles SET kilometros=8008 WHERE matricula="4783CBJ";
-UPDATE automoviles SET kilometros=24796 WHERE matricula="5031BHL";
-UPDATE automoviles SET kilometros=28500 WHERE matricula="5573BFS";
-UPDATE automoviles SET kilometros=7659 WHERE matricula="5678BRZ";
-UPDATE automoviles SET kilometros=25672 WHERE matricula="6761CYM";
-UPDATE automoviles SET kilometros=1200 WHERE matricula="7600CFG";
-UPDATE automoviles SET kilometros=15873 WHERE matricula="7839CDR";
-UPDATE automoviles SET kilometros=35978 WHERE matricula="7856BLD";
-UPDATE automoviles SET kilometros=46980 WHERE matricula="8795BTK";
+UPDATE automoviles INNER JOIN contratos ON matricula=matricula_automoviles
+    SET kilometros=kfinal WHERE numcontrato=15;
 
 UPDATE automoviles
     SET precio=precio*1.02;
 
--- UPDATE automoviles NATURAL JOIN contratos
---     SET kilometros=kfinal WHERE ffinal BETWEEN "2017-03-04" AND "2017-04-15";
+UPDATE automoviles INNER JOIN contratos ON matricula=matricula_automoviles
+    SET kilometros=kfinal WHERE ffinal BETWEEN "2017-03-04" AND "2017-04-15";
