7296b54a727374ab4286012c5282c9a197a6465c
Qv1ko <vicgarmur947@gmail.com>
2023-01-26 13:17:00 +0100

    Ejercicio 8 MySQL

---
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio8_p8.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE ejercicio8_p8;
+
+CREATE TABLE usuarios(
+    id SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    curso SMALLINT(5) UNSIGNED DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO usuarios(id,nombre,curso)
+    VALUES(1,"Paula",1),
+        (2,"Silvia",1),
+        (3,"Carlos",2),
+        (4,"Ruth",5),
+        (5,"José Antonio",NULL);
+
+CREATE TABLE cursos(
+    id SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+	nombre VARCHAR(50) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO cursos(id,nombre)
+    VALUES(1,"HTML5"),
+        (2,"CSS"),
+        (3,"JavaScript"),
+        (4,"PHP"),
+        (5,"MySQL");
+
+ALTER TABLE usuarios ADD FOREIGN KEY(curso) REFERENCES cursos(id) ON UPDATE CASCADE;
+
+SELECT usuarios.nombre,cursos.nombre FROM usuarios INNER JOIN cursos on usuarios.curso=cursos.id;
+
+SELECT usuarios.nombre,cursos.nombre FROM usuarios LEFT JOIN cursos on usuarios.curso=cursos.id;
+
+SELECT usuarios.nombre,cursos.nombre FROM usuarios RIGHT JOIN cursos on usuarios.curso=cursos.id;
+
+SELECT usuarios.nombre,cursos.nombre FROM cursos LEFT JOIN usuarios on usuarios.curso=cursos.id;
+
+SELECT usuarios.nombre,cursos.nombre FROM usuarios LEFT JOIN cursos on usuarios.curso=cursos.id 
+    UNION SELECT usuarios.nombre,cursos.nombre FROM usuarios RIGHT JOIN cursos on usuarios.curso=cursos.id;
+
+mysqldump -u root -p ejercicio8_p8 > "C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Unidad2-SQL\\Ejercicios\\Ejercicio8_p8_backup.sql"
+
+DROP DATABASE ejercicio8_p8;
+
+CREATE DATABASE ejercicio8_p8;
+
+USE ejercicio8_p8;
+
+SOURCE C:/Users/Usuario/DAM.git/DAM/Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio8_p8_backup.sql
---
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio8_p8_backup.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.16  Distrib 10.1.40-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: ejercicio8_p8
+-- ------------------------------------------------------
+-- Server version	10.1.40-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cursos` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'HTML5'),(2,'CSS'),(3,'JavaScript'),(4,'PHP'),(5,'MySQL');
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `curso` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `curso` (`curso`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`curso`) REFERENCES `cursos` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Paula',1),(2,'Silvia',1),(3,'Carlos',2),(4,'Ruth',5),(5,'Jos├® Antonio',NULL);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-26 13:05:22
