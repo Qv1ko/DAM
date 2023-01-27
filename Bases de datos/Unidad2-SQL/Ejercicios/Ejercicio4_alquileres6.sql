c73c5bb0ad7f5a9ef60e2f9be9abefb2ebd3bb91
Qv1ko <vicgarmur947@gmail.com>
2023-01-27 13:08:06 +0100

    Correción y ejercicio 9

--- Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres6.sql
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres6.sql
@@ -1,2 +1,17 @@
 START TRANSACTION;
 
+DELETE FROM contratos WHERE numcontrato=40;
+
+DELETE FROM contratos WHERE numcontrato>24;
+
+DELETE FROM contratos WHERE matricula_automoviles IN (SELECT matricula FROM automoviles WHERE marca_marcas="BMW");
+
+DELETE FROM contratos WHERE dni_clientes IN (SELECT dni FROM clientes WHERE apellidos="Fuentes Rojas");
+
+DELETE FROM clientes WHERE apellidos="FUentes Rojas";
+
+DELETE FROM contratos 
+    WHERE dni_clientes IN (SELECT dni FROM clientes WHERE nombre="Ismael" AND apellidos="Poza Rincón") AND 
+        matricula_automoviles IN (SELECT matricula FROM automoviles WHERE marca_marcas="Audi" AND modelo="A4");
+
+-- DELETE FROM automoviles WHERE precio IN (marca_marcas="Mercedes" AND modelo="500 E");
--- Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio8_p8.sql
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio8_p8.sql
@@ -42,11 +42,7 @@ SELECT usuarios.nombre,cursos.nombre FROM usuarios LEFT JOIN cursos on usuarios.
     UNION SELECT usuarios.nombre,cursos.nombre FROM usuarios RIGHT JOIN cursos on usuarios.curso=cursos.id;
 
 mysqldump -u root -p ejercicio8_p8 > "C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Unidad2-SQL\\Ejercicios\\Ejercicio8_p8_backup.sql"
-
 DROP DATABASE ejercicio8_p8;
-
 CREATE DATABASE ejercicio8_p8;
-
 USE ejercicio8_p8;
-
 SOURCE C:/Users/Usuario/DAM.git/DAM/Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio8_p8_backup.sql
