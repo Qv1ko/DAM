1b4183a8feb7261c7ec54998ff6e2f7402af9964
Qv1ko <vicgarmur947@gmail.com>
2023-01-28 21:56:52 +0100

    Ejercicio zoo completado

--- Programación/Java/src/paqueteEjercicios2/Ejercicio09_Informe.java
+++ Programación/Java/src/paqueteEjercicios2/Ejercicio09_Informe.java
@@ -13,27 +13,60 @@ public class Ejercicio09_Informe {
     }//main
 
     private static void informe(Ejercicio09_Zoo[] animales) {
-        String[] habitats={"Tierra","Mar","Aire"};
-        // String[] comidas={"Carne","Pescado","Pienso"};
-        // double carne=0.0,pescado=0.0,pienso=0.0;
-        for(int i=0;i<animales.length;i++) {
-            for(int j=0;j<habitats.length;j++) {
-                if(animales[i].getHabitat().equalsIgnoreCase(habitats[j])) {
-                    System.out.println("\nAnimales de "+habitats[j].toLowerCase()+":");
-                    System.out.println(animales[i]);
-                }
+        String habitatTierra="",habitatMar="",habitatAire="";
+        double comidaCarne=0.0,comidaPescado=0.0,comidaPienso=0.0;
+        float costesTerrestres=0.0f,costesJaulas=0.0f,costesEstanques=0.0f,costeTerrestre=15.0f,costeJaula=20.0f,costeEstanques=45.0f;
+        for(Ejercicio09_Zoo animal:animales) {
+            if(animal.getHabitat().equalsIgnoreCase("tierra")) {
+                habitatTierra+=animal+"\n";
+                costesTerrestres+=costeTerrestre;
+            } else if(animal.getHabitat().equalsIgnoreCase("mar")) {
+                habitatMar+=animal+"\n";
+                costesJaulas+=costeJaula;
+            } else if(animal.getHabitat().equalsIgnoreCase("aire")) {
+                habitatAire+=animal+"\n";
+                costesEstanques+=costeEstanques;
             }
-            // for(int k=0;k<comidas.length;k++) {
-            //     if(animales[i].getComida().equalsIgnoreCase(comidas[k])) {
-
-            //     }
-            // }
-            
+            if(animal.getComida().equalsIgnoreCase("carne")) {
+                comidaCarne+=animal.getKgDiarios()*7;
+            } else if(animal.getComida().equalsIgnoreCase("pescado")) {
+                comidaPescado+=animal.getKgDiarios()*7;
+            } else if(animal.getComida().equalsIgnoreCase("pienso")) {
+                comidaPienso+=animal.getKgDiarios()*7;
+            }
         }
+        System.out.println("Animales de habitat terrestre:\n"+habitatTierra+"\nAnimales de habitat marino:\n"+habitatMar+"\nAnimales de habitat aereo:\n"+habitatAire);
+        System.out.println("Carne semanal -> "+comidaCarne+" kg"+"\nPescado semanal -> "+comidaPescado+" kg"+"\nPienso semanal -> "+comidaPienso+" kg\n");
+        System.out.println("Mantenimiento de jaulas terrestes -> "+costesTerrestres+" euros\nMantenimiento de jaulas -> "+costesJaulas+" euros\nMantenimiento de estanques -> "+costesEstanques+" euros");
     }//informe
 
     private static void informeExcel(Ejercicio09_Zoo[] animales) {
-        // String[][] datosInforme=new Ejercicio09_Zoo;
+        String[][] datosInforme=new String[4][4];
+        double costeTerrestre=15.0f,costeJaula=20.0f,costeEstanques=45.0f;
+        for(int i=0;i<datosInforme.length;i++) {
+            for(int j=0;j<datosInforme[i].length;j++) {
+                datosInforme[0][j]=(i==0&&j==0)? "Habitat":(i==0&&j==1)? "Especies":(i==0&&j==2)? "Kg semanales":(i==0&&j==3)? "Mantenimiento de jaulas":"";
+                if(i==1) {
+                    datosInforme[i][j]=(j==0)? "Terrestre":(j==1)? Integer.toString(cantAnimales(animales,"tierra")):(j==2)? "Kg semanales":(j==3)? Double.toString(cantAnimales(animales,"tierra")*costeTerrestre)+" euros":"";
+                } else if(i==2) {
+                    datosInforme[i][j]=(j==0)? "Marino":(j==1)? Integer.toString(cantAnimales(animales,"mar")):(j==2)? "Kg semanales":(j==3)? Double.toString(cantAnimales(animales,"mar")*costeEstanques)+" euros":"";
+                } else if(i==3) {
+                    datosInforme[i][j]=(j==0)? "Aereo":(j==1)? Integer.toString(cantAnimales(animales,"aire")):(j==2)? "Kg semanales":(j==3)? Double.toString(cantAnimales(animales,"aire")*costeJaula)+" euros":"";
+                }
+                System.out.print("| "+datosInforme[i][j]+" |");
+            }
+            System.out.println();
+        }
     }//informeExcel
+
+    private static int cantAnimales(Ejercicio09_Zoo[] animales,String habitat) {
+        int numAnimales=0;
+        for(Ejercicio09_Zoo animal:animales) {
+            if(animal.getHabitat().equalsIgnoreCase(habitat)) {
+                numAnimales++;
+            }
+        }
+        return numAnimales;
+    }//cantAnimales
 
 }//class
