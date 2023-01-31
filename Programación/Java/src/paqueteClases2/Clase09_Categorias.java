1fc095b1102d0f0fd901dfc5423d5d4af9aca965
Qv1ko <vicgarmur947@gmail.com>
2023-01-31 11:00:36 +0100

    Amachon constructores, getters y setters

---
+++ Programación/Java/src/paqueteClases2/Clase09_Categorias.java
@@ -0,0 +1,5 @@
+package paqueteClases2;
+
+public enum Clase09_Categorias {
+    
+}//enum
---
+++ Programación/Java/src/paqueteClases2/Clase09_CestaAmachon.java
@@ -0,0 +1,9 @@
+package paqueteClases2;
+
+public class Clase09_CestaAmachon {
+    
+    public static void main(String[] args) {
+        // String[][] productos;
+    }//main
+
+}//class
