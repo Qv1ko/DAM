ca1ecebe92d234f802f34470d5fa18a4437ef5cf
Qv1ko <vicgarmur947@gmail.com>
2023-02-11 22:14:57 +0100

    Anunciados ejercicio 1 y 2

--- Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres1.sql
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres1.sql
@@ -1,14 +1,25 @@
+--2. Ejecuta MySQL como administrador
+mysql –u root –p
+
+--3. Crea una base de datos llamada alquileres
 CREATE DATABASE ejercicio4_alquileres;
 
+--4. Activa como base de datos de trabajo la base de datos alquileres
 USE ejercicio4_alquileres;
 
+--5. Comprueba las tablas que hay en alquileres
 SHOW TABLES;
 
+--6. Crea la tabla marcas con las columnas numero de tipo tinyint, marca, siendo la columna numero clave principal
 CREATE TABLE marcas(
     marca VARCHAR(15) PRIMARY KEY UNIQUE,
     numero CHAR(2)
 );
 
+/*7. Crea la tabla automoviles con las columnas matricula, marca, modelo, color, precio, kilometros, extras, 
+alquilado, siendo la columna matricula clave principal, la columna marca relacionada con la columna marca de la 
+tabla marcas, no admitiendo valores nulos en marca, modelo y precio, y tomando la columna alquilado como valor 
+por defecto el valor False */
 CREATE TABLE automoviles(
     matricula CHAR(7) PRIMARY KEY,
     marca_marcas VARCHAR(15) NOT NULL,
@@ -21,12 +32,15 @@ CREATE TABLE automoviles(
     FOREIGN KEY(marca_marcas) REFERENCES marcas(marca)
 );
 
+--8. Muestra las tablas que hay en la base de datos
 SHOW TABLES;
 
+--9. Muestra la estructura de cada una de las tablas
 DESC automoviles;
-
 DESC marcas;
 
+/*10. Crea la tabla clientes con las columnas dni, nombre, apellidos, direccion, localidad, carnet, fechaexp. 
+La clave principal es dni. No se admiten nulos en las columnas nombre, apellidos, carnet y fechaexp */
 CREATE TABLE clientes(
     dni CHAR(9) PRIMARY KEY,
     apellidos VARCHAR(25) NOT NULL,
@@ -37,6 +51,9 @@ CREATE TABLE clientes(
     fechaexp DATE NOT NULL
 );
 
+/*11. Crea la tabla contratos con las columnas dni, matricula, finicial, ffinal, kinicial, kfinal. La columna 
+numcontrato es clave principal, numérica de 5 cifras y autoincrementable. Las columnas dni y matricula están 
+relacionadas con las columnas correspondientes de las tablas automoviles y clientes */
 CREATE TABLE contratos(
     numcontrato INT(5) PRIMARY KEY AUTO_INCREMENT,
     matricula_automoviles CHAR(7) NOT NULL,
@@ -49,39 +66,33 @@ CREATE TABLE contratos(
     FOREIGN KEY(matricula_automoviles) REFERENCES automoviles(matricula)
 );
 
+--12. Crea una tabla contratos2 con la misma estructura que la anterior
 CREATE TABLE contratos2 LIKE contratos;
 
+--13. Comprueba todas las tablas que hay en la base de datos y su estructura
 SHOW TABLES;
-
 DESC automoviles;
-
 DESC clientes;
-
 DESC contratos;
-
 DESC contratos2;
-
 DESC marcas;
 
+--14. Carga archivos de texto en cada una de las tablas
 LOAD DATA LOCAL INFILE 'C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Evaluacion2\\SQL\\Ejercicio4_txts\\marcas.txt'
     INTO TABLE marcas
     LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
-
 LOAD DATA LOCAL INFILE 'C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Evaluacion2\\SQL\\Ejercicio4_txts\\automoviles.txt'
     INTO TABLE automoviles
     FIELDS TERMINATED BY '\t'
     LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
-
 LOAD DATA LOCAL INFILE 'C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Evaluacion2\\SQL\\Ejercicio4_txts\\clientes.txt'
     INTO TABLE clientes
     FIELDS TERMINATED BY '\t'
     LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
-
 LOAD DATA LOCAL INFILE 'C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Evaluacion2\\SQL\\Ejercicio4_txts\\contratos.txt'
     INTO TABLE contratos
     FIELDS TERMINATED BY '\t'
     LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
-
 LOAD DATA LOCAL INFILE 'C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Evaluacion2\\SQL\\Ejercicio4_txts\\contratos2.txt'
     INTO TABLE contratos2
     FIELDS TERMINATED BY '\t'
@@ -94,6 +105,7 @@ LOAD DATA LOCAL INFILE 'C:\\Users\\Usuario\\DAM.git\\DAM\\Bases de datos\\Evalua
     FIELDS TERMINATED BY '\t'
     LINES TERMINATED BY '\r\n' IGNORE 1 LINES;
 
+--15. Inserta contratos en la tabla contratos
 INSERT INTO contratos(matricula_automoviles,dni_clientes,finicial,kinicial)
     VALUES("1234BMY", "37409800", "2009-07-10", 22561),
     ("2058CGF", "00371569", "2009-07-11", 9736),
@@ -103,18 +115,19 @@ INSERT INTO contratos(matricula_automoviles,dni_clientes,finicial,kinicial)
     ("8795BTK", "12348630", "2009-11-16", 46980),
     ("2123BTB", "43809540", "2009-11-16", 34323);
 
+--16. Comprueba que se ha insertado la fila y el número de contrato
 SELECT * FROM contratos;
 
+--17. Pasa a usar la base de datos test. ¿Qué tablas contiene?
 USE test;
-
 SHOW TABLES;
 
+--18. Pasa de nuevo a trabajar con la base alquileres
+USE ejercicio4_alquileres;
 SHOW CREATE TABLE marcas;
-
 SHOW CREATE TABLE automoviles;
-
 SHOW CREATE TABLE clientes;
-
 SHOW CREATE TABLE contratos;
 
+--19. Cierra la sesión
 exit
--- Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres2.sql
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio4_alquileres2.sql
@@ -1,54 +1,90 @@
+--1. Obtener todos los datos de los coches la tabla automóviles
 SELECT * FROM automoviles;
 
+--2. Obtener todos los datos de los clientes
 SELECT * FROM clientes;
 
+--3. Obtener todos los datos de los contratos realizados
 SELECT * FROM contratos;
 
+--4. Listar los apellidos y nombres de todos los clientes
 SELECT apellidos,nombre FROM clientes;
-
+/*5. Realizar un listado, ordenado ascendentemente por apellidos y después por nombre, con 
+los apellidos y nombres de todos los clientes */
 SELECT apellidos,nombre FROM clientes ORDER BY apellidos ASC;
 
+/*6. Listar las matrículas de los coches contratados y fechas iniciales de contrato ordenado 
+por fecha inicial */
 SELECT matricula_automoviles,finicial FROM contratos ORDER BY finicial ASC;
 
+--7. Obtener una lista sin repeticiones con las matrículas de los coches contratados
 SELECT DISTINCT matricula_automoviles FROM contratos;
 
+--8. Obtener una lista con los nombres y apellidos de los clientes que son de Toledo
 SELECT nombre,apellidos FROM clientes WHERE localidad="Toledo";
 
+/*9. Obtener una lista con los nombres y apellidos de todos los clientes de Madrid con carnet 
+tipo C1 */
 SELECT nombre,apellidos FROM clientes WHERE localidad="Madrid" AND carnet="C1";
 
+--10. Obtener los datos del cliente con número de DNI 00740365
 SELECT * FROM clientes WHERE dni="00740365";
 
+/*11. Obtener la matrícula y fecha inicial de contrato de todos los coches contratados antes 
+del 25/mar/08 */
 SELECT matricula_automoviles,finicial FROM contratos WHERE finicial<"2008-03-25";
 
+--12. Obtener todos los datos de los contratos realizados el día 24/mar/09
 SELECT * FROM contratos WHERE finicial="2009-11-24";
 
+/*13. Obtener la matrícula, fecha inicial, y fecha final de contrato de los coches 
+contratados en marzo de 2008 */
 SELECT matricula_automoviles,finicial,ffinal FROM contratos WHERE finicial>="2008-03-01" AND finicial<="2008-03-31";
 SELECT matricula_automoviles,finicial,ffinal FROM contratos WHERE finicial BETWEEN "2008-03-01" AND "2008-03-31";
 
+/*14. Obtener la matrícula, fecha inicial, y fecha final de contrato de los coches 
+contratados en marzo de 2008 usando las funciones MONTH y YEAR */
 SELECT matricula_automoviles,finicial,ffinal FROM contratos WHERE MONTH(finicial)=3 AND YEAR(finicial)=2008;
 
+--15. Obtener la matrícula, marca y modelo de todos los coches disponibles
 SELECT matricula,marca_marcas,modelo FROM automoviles WHERE alquilado=false;
 
+/*16. Obtener la matrícula, marca y modelo de todos los coches disponibles con menos de 
+20000 kilómetros */
 SELECT matricula,marca_marcas,modelo FROM automoviles WHERE alquilado=false AND kilometros<20000;
 
+/*17. Listar los nombres y apellidos de todos los clientes cuyo primer apellido comienza 
+por ‘B’ */
 SELECT nombre,apellidos FROM clientes WHERE apellidos LIKE "B%";
 
+--18. Obtener todos los datos del contrato número 19
 SELECT * FROM contratos WHERE numcontrato=19;
 
+/*19. Obtener todos los datos de los contratos con número comprendido entre 7 y 12, y el 
+contrato número 19 */
 SELECT * FROM contratos WHERE numcontrato>=7 AND numcontrato<=12 OR numcontrato=19;
 SELECT * FROM contratos WHERE numcontrato IN (7,8,9,10,11,12,19);
 
+/*20. Obtener los números de contrato finalizados, las fechas de inicio y fin y los 
+kilómetros iniciales y finales */
 SELECT numcontrato,finicial,ffinal,kinicial,kfinal FROM contratos WHERE ffinal IS NOT NULL;
 
+/*21. Modificar los encabezados del anterior listado para que en vez de finicial aparezca 
+fecha inicial de contrato y en lugar de ffinal, kinicial y kfinal lo correspondiente */
 SELECT numcontrato,finicial AS "Fecha inicial",ffinal AS "Fecha final",kinicial AS "Kms iniciales",kfinal AS "Kms finales"
     FROM contratos
     WHERE ffinal IS NOT NULL;
 
+/*22. Obtener DNI, nombre y apellidos de todos los clientes que tienen expedido el carnet de 
+conducir hace más de 10 años */
 SELECT dni,nombre,apellidos FROM clientes WHERE YEAR(CURDATE())-YEAR(fechaexp);
 
-SELECT marca,modelo
-
+/*23. Obtener un listado con la marca y modelo en una misma columna de todos los coches 
+disponibles ordenado por marca y por modelo ascendentemente */
 SELECT CONCAT(marca_marcas," ",modelo) AS coche FROM automoviles ORDER BY marca_marcas,modelo ASC;
 --SELECT CONCAT(marca_marcas," ",modelo) AS coche FROM automoviles ORDER BY marca_marcas DESC, modelo DESC;
 
-SELECT CONCAT("El cliente: ",dni_clientes," Contrato el: ",matricula_automoviles) AS "Contratos realizados" FROM contratos;
+/*24. Obtener un listado con el encabezado ‘Contratos realizados’ que contenga una línea por
+cada contrato realizado con el contendido El cliente con DNI ... contrató el coche con 
+matricula ... */
+SELECT CONCAT("El cliente con DNI ",dni_clientes," contrató el coche con matricula ",matricula_automoviles) AS "Contratos realizados" FROM contratos;
