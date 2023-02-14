5879279e4b37ef9174ae85121d43be3476b2e0ac
Qv1ko <vicgarmur947@gmail.com>
2023-02-14 17:17:23 +0100

    Examen 3 Programación

---
+++ Programación/Java/src/paqueteExamenes/examen3/pkt1/Main.java
@@ -0,0 +1,43 @@
+package paqueteExamenes.examen3.pkt1;
+
+import java.util.ArrayList;
+
+import paqueteExamenes.examen3.pkt2.ClaseExterna;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        ArrayList<Empleados> empleados=new ArrayList<Empleados>();
+        empleados.add(new Empleados("12346788T","Victor Garcia",ClaseExterna.cargoRandom(),2018,1200.5f));
+        empleados.add(new Empleados("99999999R","Juan Fernandez",ClaseExterna.cargoRandom(),2007,1100));
+        empleados.add(new Empleados("12345093L","Manuel Rodrigez",ClaseExterna.cargoRandom(),2014,1085));
+        empleados.add(new Empleados("73003057E","Andres Calleja",ClaseExterna.cargoRandom(),2011,1235.83f));
+        empleados.add(new Empleados("14611671K","Ana Pascual",ClaseExterna.cargoRandom(),2000,1048.59f));
+        empleados.add(new Empleados("18317922E","Rodrigo Diaz",ClaseExterna.cargoRandom(),2020,1188.75f));
+        antiguosEmpleados(empleados);
+        maxNomina(empleados);
+    }//main
+
+    public static void antiguosEmpleados(ArrayList<Empleados> empleados) {
+        ArrayList<Empleados> antiguos=new ArrayList<Empleados>();
+        for(Empleados empleado:empleados) {
+            if(empleado.getAnoActual()-empleado.getAnoAlta()>9) {
+                antiguos.add(empleado);
+            }
+        }
+        System.out.println("\n\nEmpleados con más de 10 años en la empresa:\n");
+        for(Empleados empleado: antiguos) {
+            System.out.println(empleado);
+        }
+    }//antiguosEmpleados
+
+    public static void maxNomina(ArrayList<Empleados> empleados) {
+        Empleados maximaNomina=new Empleados();
+        for(Empleados empleado:empleados) {
+            maximaNomina=(empleado.getNomina()>maximaNomina.getNomina())? empleado:maximaNomina;
+        }
+        System.out.println("\nEmpleado con la nómina más alta:\n");
+        System.out.println(maximaNomina);
+    }//maxNomina
+
+}//class
---
+++ Programación/Java/src/paqueteExamenes/examen3/pkt2/ClaseExterna.java
@@ -0,0 +1,57 @@
+package paqueteExamenes.examen3.pkt2;
+
+public class ClaseExterna {
+    
+    public static void validarDni(String dni) {
+        char letraDni=dni.charAt(dni.length()-1),letraResultado=' ';
+        String nif="";
+        boolean validacionError=false;
+        for(int i=0;i<dni.length()-1;i++) {
+            if(Character.isDigit(dni.charAt(i))) {
+                nif+=Character.getNumericValue(dni.charAt(i));    
+            }
+        }
+        if(nif.length()==8) {
+            switch(Integer.parseInt(nif)%23) {
+                case 0 -> letraResultado='T';
+                case 1 -> letraResultado='R';
+                case 2 -> letraResultado='W';
+                case 3 -> letraResultado='A';
+                case 4 -> letraResultado='G';
+                case 5 -> letraResultado='M';
+                case 6 -> letraResultado='Y';
+                case 7 -> letraResultado='F';
+                case 8 -> letraResultado='P';
+                case 9 -> letraResultado='D';
+                case 10 -> letraResultado='X';
+                case 11 -> letraResultado='B';
+                case 12 -> letraResultado='N';
+                case 13 -> letraResultado='J';
+                case 14 -> letraResultado='Z';
+                case 15 -> letraResultado='S';
+                case 16 -> letraResultado='Q';
+                case 17 -> letraResultado='V';
+                case 18 -> letraResultado='H';
+                case 19 -> letraResultado='L';
+                case 20 -> letraResultado='C';
+                case 21 -> letraResultado='K';
+                case 22 -> letraResultado='E';
+            }
+            if(letraDni!=letraResultado) {
+                validacionError=true;
+            }
+        } else {
+            validacionError=true;
+        }
+        if(validacionError) {
+            System.out.println("DNI incorrecto");
+        }
+        // System.out.println((validacionError)? "DNI incorrecto":"");
+    }//validarDni
+
+    public static String cargoRandom() {
+        String[] cargos=new String[]{"Ceo","Encargad@","Operari@","Secretari@"};
+        return cargos[(int)(Math.random()*4)];
+    }//cargoRandom
+
+}//class
