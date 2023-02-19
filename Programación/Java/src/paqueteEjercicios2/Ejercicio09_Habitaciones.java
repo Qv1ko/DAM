3c27246f2bff1f4f9a15904c62f180fcb60ea71b
Qv1ko <vicgarmur947@gmail.com>
2023-02-19 21:03:02 +0100

    Ejercicio programación

--- Programación/Java/src/paqueteEjercicios2/Ejercicio09_Habitaciones.java
+++ Programación/Java/src/paqueteEjercicios2/Ejercicio09_Habitaciones.java
@@ -2,4 +2,47 @@ package paqueteEjercicios2;
 
 public class Ejercicio09_Habitaciones {
 
+    private String habitacion;
+    private boolean terraza;
+    private int precio,id=100;
+
+    public Ejercicio09_Habitaciones(String habitacion,boolean terraza) {
+        this.habitacion=habitacion;
+        this.terraza=terraza;
+        id++;
+    }
+
+    public String getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(String habitacion) {
+        this.habitacion=habitacion;
+    }
+
+    public boolean isTerraza() {
+        return terraza;
+    }
+
+    public void setTerraza(boolean terraza) {
+        this.terraza=terraza;
+    }
+
+    public int getPrecio() {
+        if(getHabitacion().equalsIgnoreCase("matrimonio")) {
+            precio=(terraza)? 40:30;
+        } else if(getHabitacion().equalsIgnoreCase("individual")) {
+            precio=(terraza)? 25:20;
+        }
+        return precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String toString() {
+        return "Habitacion "+getId()+" - "+getHabitacion()+"\n\sPrecio: "+getPrecio()+" euros/dia";
+    }
+
 }
--- Programación/Java/src/paqueteEjercicios2/Ejercicio09_Hotel.java
+++ Programación/Java/src/paqueteEjercicios2/Ejercicio09_Hotel.java
@@ -1,5 +1,20 @@
 package paqueteEjercicios2;
 
+import java.util.ArrayList;
+
 public class Ejercicio09_Hotel {
     
-}
+    public static void main(String[] args) {
+        ArrayList<Ejercicio09_Habitaciones> habitaciones=new ArrayList<Ejercicio09_Habitaciones>();
+        habitaciones.add(new Ejercicio09_Habitaciones("Individual",true));
+        habitaciones.add(new Ejercicio09_Habitaciones("Individual",false));
+        habitaciones.add(new Ejercicio09_Habitaciones("Individual",false));
+        habitaciones.add(new Ejercicio09_Habitaciones("Matrimonio",true));
+        habitaciones.add(new Ejercicio09_Habitaciones("Matrimonio",false));
+        habitaciones.add(new Ejercicio09_Habitaciones("Matrimonio",false));
+        for(Ejercicio09_Habitaciones habitacion:habitaciones) {
+            System.out.println(habitacion);
+        }
+    }//main
+
+}//class
