c0f46ff38e2ddc2ac6f7f65018cfe5c7d098533b
Qv1ko <vicgarmur947@gmail.com>
2023-02-20 01:36:30 +0100

    De clase a enum

--- Programación/Java/src/paqueteEjercicios2/Ejercicio09_Habitaciones.java
+++ Programación/Java/src/paqueteEjercicios2/Ejercicio09_Habitaciones.java
@@ -1,33 +1,34 @@
 package paqueteEjercicios2;
 
-public class Ejercicio09_Habitaciones {
+public enum Ejercicio09_Habitaciones {
+
+    HABIT(101,"Individual",true),HABI1(102,"Individual",false),HABI2(103,"Individual",false),HABMT(104,"Matrimonio",true),HABM1(105,"Matrimonio",false),HABM2(106,"Matrimonio",false);
 
     private String habitacion;
-    private boolean terraza;
+    private boolean terraza,alquilado;
     private int precio,id;
-    private static int totalId=100;
 
-    public Ejercicio09_Habitaciones(String habitacion,boolean terraza) {
+    private Ejercicio09_Habitaciones(int id,String habitacion,boolean terraza) {
         this.habitacion=habitacion;
         this.terraza=terraza;
-        this.id=++totalId;
-    }
+        this.id=id;
+    }//Ejercicio09_Habitaciones
 
     public String getHabitacion() {
         return habitacion;
-    }
-
-    public void setHabitacion(String habitacion) {
-        this.habitacion=habitacion;
-    }
+    }//getHabitacion
 
     public boolean isTerraza() {
         return terraza;
-    }
+    }//isTerraza
 
-    public void setTerraza(boolean terraza) {
-        this.terraza=terraza;
-    }
+    public void setAlquilado(boolean alquilado) {
+        this.alquilado = alquilado;
+    }//setAlquilado
+
+    public boolean isAlquilar() {
+        return alquilado;
+    }//isAlquilar
 
     public int getPrecio() {
         if(getHabitacion().equalsIgnoreCase("matrimonio")) {
@@ -36,14 +37,26 @@ public class Ejercicio09_Habitaciones {
             precio=(terraza)? 25:20;
         }
         return precio;
-    }
+    }//getPrecio
 
     public int getId() {
         return id;
-    }
+    }//getId
+
+    public String getNumCamas() {
+        return (getHabitacion().equalsIgnoreCase("individual"))? " 2 camas individuales ":" 1 cama de matrimonio";
+    }//getNumCamas
+
+    public String getTipoHabitacion() {
+        return (isTerraza())? " con terraza y":" con ventana y";
+    }//getTipoHabitacion
+
+    public String getDisposicion() {
+        return (isAlquilar())? "No disponible\n":"Disponible\n";
+    }//getDisposicion
 
     public String toString() {
-        return "Habitacion "+getId()+" - "+getHabitacion()+"\n\sPrecio: "+getPrecio()+" euros/dia";
-    }
+        return "Habitacion "+getId()+getTipoHabitacion()+getNumCamas()+"\n\sPrecio: "+getPrecio()+" euros/dia\n";
+    }//toString
 
 }//class
--- Programación/Java/src/paqueteEjercicios2/Ejercicio09_Hotel.java
+++ Programación/Java/src/paqueteEjercicios2/Ejercicio09_Hotel.java
@@ -6,14 +6,14 @@ public class Ejercicio09_Hotel {
     
     public static void main(String[] args) {
         ArrayList<Ejercicio09_Habitaciones> habitaciones=new ArrayList<Ejercicio09_Habitaciones>();
-        habitaciones.add(new Ejercicio09_Habitaciones("Individual",true));
-        habitaciones.add(new Ejercicio09_Habitaciones("Individual",false));
-        habitaciones.add(new Ejercicio09_Habitaciones("Individual",false));
-        habitaciones.add(new Ejercicio09_Habitaciones("Matrimonio",true));
-        habitaciones.add(new Ejercicio09_Habitaciones("Matrimonio",false));
-        habitaciones.add(new Ejercicio09_Habitaciones("Matrimonio",false));
+        habitaciones.add(Ejercicio09_Habitaciones.HABIT);
+        habitaciones.add(Ejercicio09_Habitaciones.HABI1);
+        habitaciones.add(Ejercicio09_Habitaciones.HABI2);
+        habitaciones.add(Ejercicio09_Habitaciones.HABMT);
+        habitaciones.add(Ejercicio09_Habitaciones.HABM1);
+        habitaciones.add(Ejercicio09_Habitaciones.HABM2);
         for(Ejercicio09_Habitaciones habitacion:habitaciones) {
-            System.out.println(habitacion);
+            System.out.println(habitacion+habitacion.getDisposicion());
         }
     }//main
 
