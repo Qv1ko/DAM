4d3f43d9b5d430d9b0a578e37d135311aca52178
Qv1ko <vicgarmur947@gmail.com>
2023-03-24 18:08:51 +0100

    Ejercicio fecha aleatoria finalizado

--- Programación/Java/src/paqueteEjercicios3/Ejercicio05_FechaRandom.java
+++ Programación/Java/src/paqueteEjercicios3/Ejercicio05_FechaRandom.java
@@ -1,11 +1,12 @@
 package paqueteEjercicios3;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 
 public class Ejercicio05_FechaRandom {
     
     public static void main(String[] args) {
-        System.out.println("La fecha aleatorio es "+genFecha());
+            System.out.println("La fecha aleatorio es "+genFecha());
     }//main
 
     private static LocalDate genFecha() {
@@ -13,13 +14,20 @@ public class Ejercicio05_FechaRandom {
         boolean flag=false;
         do {
             try {
-                fecha=LocalDate.of((int)(Math.random()*(LocalDate.now().getYear()+1)),(int)(Math.random()*12)+1,(int)(Math.random()*31)+1);
-                if(fecha.getDayOfMonth()>28&&fecha.getMonth().getValue()==2) {
-                    throw new Exception("\nFecha erronea\n");
-                }
+                fecha=LocalDate.of((int)(Math.random()*(LocalDate.now().getYear())+1),(int)(Math.random()*12)+1,(int)(Math.random()*31)+1);
+                // if(fecha.getDayOfMonth()>28&&fecha.getMonth().getValue()==2) {
+                //     throw new Exception("\tFecha erronea");
+                // } else if(fecha.getDayOfMonth()>29&&fecha.getMonth().getValue()==2&&fecha.isLeapYear()) {
+                //     throw new Exception("\tFecha erronea");
+                // } else if(fecha.getDayOfMonth()>30&&(fecha.getMonth().getValue()==4||fecha.getMonth().getValue()==6||fecha.getMonth().getValue()==9||fecha.getMonth().getValue()==11)) {
+                //     throw new Exception("\tFecha erronea");
+                // }
                 flag=false;
-            } catch(Exception exc) {
-                System.out.println(exc.getMessage());
+            // } catch(Exception exc) {
+            //     System.out.println(exc.getMessage());
+            //     flag=true;
+            } catch(DateTimeException exc) {
+                System.out.println("\tFecha erronea");
                 flag=true;
             }
         } while(flag);
