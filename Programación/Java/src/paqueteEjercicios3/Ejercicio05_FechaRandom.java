dedb7c5f82046d56fd796ba6f2c8f2850e79a45b
Qv1ko <vicgarmur947@gmail.com>
2023-03-27 12:58:21 +0200

    Correción ejercicio fecha aleatoria

--- Programación/Java/src/paqueteEjercicios3/Ejercicio05_FechaRandom.java
+++ Programación/Java/src/paqueteEjercicios3/Ejercicio05_FechaRandom.java
@@ -7,22 +7,30 @@ import java.time.format.DateTimeFormatter;
 public class Ejercicio05_FechaRandom {
     
     public static void main(String[] args) {
-            System.out.println("La fecha aleatorio es "+genFecha());
-            System.out.println("La fecha aleatorio es "+genFechaDF());
+        System.out.println("La fecha aleatoria es "+genFecha());
+        System.out.println("La fecha aleatoria es "+genFechaDF());
+        boolean run=true;
+        while(run) {
+            try {
+                System.out.println("La fecha aleatoria es "+correccion());
+                run=false;
+            } catch(DateTimeException exc) {
+                System.out.println(exc.getMessage());
+            }
+        }
     }//main
 
     private static LocalDate genFecha() {
         LocalDate fecha=null;
-        boolean flag=false;
-        do {
+        boolean run=true;
+        while(run) {
             try {
                 fecha=LocalDate.of((int)(Math.random()*(LocalDate.now().getYear())+1),(int)(Math.random()*12)+1,(int)(Math.random()*31)+1);
-                flag=false;
+                run=false;
             } catch(DateTimeException exc) {
                 System.out.println("\tFecha erronea");
-                flag=true;
             }
-        } while(flag);
+        }
         return fecha;
     }//genFecha
 
@@ -30,8 +38,8 @@ public class Ejercicio05_FechaRandom {
         DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy");
         int dia=0,mes=0,ano=0;
         LocalDate fecha=null,leapValidator=null;
-        boolean flag=false;
-        do {
+        boolean run=true;
+        while(run) {
             try {
                 dia=(int)(Math.random()*31)+1;
                 mes=(int)(Math.random()*12)+1;
@@ -45,13 +53,21 @@ public class Ejercicio05_FechaRandom {
                     throw new Exception("\tFecha erronea");
                 }
                 fecha=LocalDate.parse((((Integer.toString(dia).length()==2)? Integer.toString(dia):("0"+dia))+"/"+((Integer.toString(mes).length()==2)? Integer.toString(mes):("0"+mes))+"/"+((Integer.toString(ano).length()==4)? Integer.toString(ano):(Integer.toString(ano).length()==3)? ("0"+ano):(Integer.toString(ano).length()==2)? ("00"+ano):("000"+ano))),df);
-                flag=false;
+                run=false;
             } catch(Exception exc) {
                 System.out.println(exc.getMessage());
-                flag=true;
             }
-        } while(flag);
+        }
         return fecha.format(df);
     }//genFechaDF
+
+    private static LocalDate correccion() throws DateTimeException {
+        LocalDate fecha=null;
+        fecha=LocalDate.of((int)(Math.random()*(LocalDate.now().getYear())+1),(int)(Math.random()*12)+1,(int)(Math.random()*31)+1);
+        if(fecha==null) {
+            throw new DateTimeException("Fecha erronea");
+        }
+        return fecha;
+    }
 
 }//class
