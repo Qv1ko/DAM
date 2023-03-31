c1dcbda29b7543d3d9c14097ca1dd5539f72ff0a
Qv1ko <vicgarmur947@gmail.com>
2023-03-31 19:54:05 +0200

    Ejercicio dir

---
+++ Programación/Java/src/paqueteEjercicios3/Ejercicio07_Dir.java
@@ -0,0 +1,13 @@
+package paqueteEjercicios3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ejercicio07_Dir {
+    
+    public static void main(String[] args) {
+        InputStreamReader input=new InputStreamReader(System.in);
+        BufferedReader buff=new BufferedReader(input);
+    }//main
+
+}//class
