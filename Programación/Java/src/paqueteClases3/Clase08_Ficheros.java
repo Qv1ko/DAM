50f158ba6333259345e63175fb5b19adb2e5a6f8
Qv1ko <vicgarmur947@gmail.com>
2023-04-03 13:56:59 +0200

    Clase ficheros

---
+++ Programación/Java/src/paqueteClases3/Clase08_Ficheros.java
@@ -0,0 +1,38 @@
+package paqueteClases3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Clase08_Ficheros {
+    
+    public static void main(String[] args) {
+        //Ruta del directorio actual
+        File dir=new File(".");
+        System.out.println(Arrays.toString(dir.listFiles()));
+        //Listar directorios
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bf=new BufferedReader(in);
+        System.out.print("Introduce el directorio: ");
+        try {
+            File directory=new File(bf.readLine());
+            leerDir(directory);
+        } catch(IOException exc) {
+            System.out.println("Entrada incorrecta - "+exc.getMessage());
+        }
+    }//main
+    
+    private static void leerDir(File directory) {
+        for(File fichero:directory.listFiles()) {
+            if(fichero.isDirectory()) {
+                System.out.println("\nSe puede escanear el contenido en "+fichero.getName());
+                leerDir(new File(fichero.toString()));
+            } else {
+                System.out.println(fichero.getName()+ " archivo de "+String.format("%.2f",fichero.length()/1024.00f)+" kb");
+            }
+        }
+    }
+
+}//class
