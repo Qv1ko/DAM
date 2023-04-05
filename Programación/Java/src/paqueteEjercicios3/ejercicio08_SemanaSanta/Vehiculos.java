42f3a173a2f726d8df6e931c1f11f6a4661ee59f
Qv1ko <vicgarmur947@gmail.com>
2023-04-05 10:13:53 +0200

    Variables vehiculos

--- Programación/Java/src/paqueteEjercicios3/ejercicio08_SemanaSanta/Vehiculos.java
+++ Programación/Java/src/paqueteEjercicios3/ejercicio08_SemanaSanta/Vehiculos.java
@@ -1,5 +1,10 @@
 package paqueteEjercicios3.ejercicio08_SemanaSanta;
 
 public class Vehiculos {
+    private String matricula;
+    private int plazas;
+    private float precio;
+    private boolean alquilado;
+
     
 }
