7f6d3fd0dad333ba31b739bdb824487300ddfe0d
Qv1ko <vicgarmur947@gmail.com>
2023-04-18 11:02:28 +0200

    Ejercicio de lectura de archivo

---
+++ Programación/Java/src/paqueteEjercicios3/Ejercicio10_ImportarFichero.java
@@ -0,0 +1,47 @@
+package paqueteEjercicios3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Ejercicio10_ImportarFichero {
+    
+    public static void main(String[] args) {
+        ArrayList<Ejercicio10_Productos> lista=new ArrayList<Ejercicio10_Productos>();
+        FileReader fr=null;
+        BufferedReader br=null;
+        // String linea="";
+        // String[] producto=null;
+        // Ejercicio10_Productos prod=null;
+        int linea=1;
+        try {
+            fr=new FileReader(new File("C:\\Users\\Usuario\\DAM\\Programación\\Java\\src\\paqueteEjercicios3\\ejercicio10_fichero\\productos.csv"));
+            br=new BufferedReader(fr);
+            br.readLine();
+            while(br.ready()) {
+                // linea=br.readLine();
+                // producto=linea.split(";");
+                // prod=new Ejercicio10_Productos(producto[0],producto[1],Integer.parseInt(producto[2]),Double.parseDouble(producto[3]));
+                try {
+                    lista.add(new Ejercicio10_Productos(br.readLine()));
+                } catch(NumberFormatException exc) {
+                    System.out.println("\n! Los valores de la linea "+linea+" del fichero son incorrectos\n");
+                }
+                linea++;
+            }
+            System.out.println(lista.size()+" productos importados");
+            for(Ejercicio10_Productos producto:lista) {
+                System.out.println(producto.toString());
+            }
+            br.close();
+        } catch(FileNotFoundException exc) {
+            System.out.println("\n! El archivo no se ha encontrado\n");
+        } catch(IOException exc) {
+            System.out.println("\n! Error en la entrada o salida\n");
+        }
+    }//main
+
+}//class
---
+++ Programación/Java/src/paqueteEjercicios3/Ejercicio10_Productos.java
@@ -0,0 +1,35 @@
+package paqueteEjercicios3;
+
+public class Ejercicio10_Productos {
+    
+    private String nombre;
+    private String desc;
+    private int cantidad;
+    private double precio;
+    
+    public Ejercicio10_Productos() {
+        this.nombre="";
+        this.desc="";
+        this.cantidad=0;
+        this.precio=0.0;
+    }//Ejercicio10_FileToArray - Constructor vacio
+
+    public Ejercicio10_Productos(String nombre,String desc,int cant,double precio) {
+        this.nombre=nombre;
+        this.desc=desc;
+        this.cantidad=cant;
+        this.precio=precio;
+    }//Ejercicio10_FileToArray
+
+    public Ejercicio10_Productos(String linea) throws NumberFormatException {
+        this.nombre=linea.split(";")[0];
+        this.desc=linea.split(";")[1];
+        this.cantidad=Integer.parseInt(linea.split(";")[2]);
+        this.precio=Double.parseDouble(linea.split(";")[3]); 
+    }//Ejercicio10_Productos - Constructor personalizado
+
+    public String toString() {
+        return "Producto: "+nombre+"\n\sDescripción: "+desc+"\n\sCantidad: "+cantidad+" unidades\n\sPrecio und: "+precio+" euros";
+    }//toString
+
+}//class
