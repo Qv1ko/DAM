8ea2d37bbde560f5a051d3c7e14c557015837a7f
Qv1ko <vicgarmur947@gmail.com>
2023-04-19 21:55:12 +0200

    Corrección de la clase y actualización del fichero

---
+++ Programación/Java/src/paqueteEjercicios3/ejercicio11_Amachon/Categorias.java
@@ -0,0 +1,34 @@
+package paqueteEjercicios3.ejercicio11_Amachon;
+
+public enum Categorias {
+	
+	CATA(21,false,0,"Categor�a A"),CATB(10,false,5,"Categor�a B"),CATC(0,true,0, "Categor�a C");
+	
+	private int impuesto;
+	private boolean envFrio;
+	private int dto;
+	private String desc;
+	
+	private Categorias(int impuesto, boolean envFrio, int dto, String desc){
+		this.impuesto = impuesto;
+		this.envFrio = envFrio;
+		this.dto = dto;
+		this.desc = desc;
+	}
+	public String getDesc() {
+		return this.desc;
+	}
+	
+	public float getDTO() {
+		return (float)(100-this.dto)/100 ; 
+	}
+	
+	public float getImpuesto() {
+		return (float)(100 + this.impuesto)/100;
+	}
+	
+	public String toString() {		
+		return this.desc +":\n\tCon el "+ this.impuesto+" % de IVA y "+ this.dto+" % de descuento \n\t"+
+				((this.envFrio)?"Necesita refrigeraci�n":"No necesita refrigeraci�n");
+	}
+}
---
+++ Programación/Java/src/paqueteEjercicios3/ejercicio11_Amachon/Validaciones.java
@@ -0,0 +1,57 @@
+package paqueteEjercicios3.ejercicio11_Amachon;
+
+public class Validaciones {
+	
+	public static boolean validarUnidades(int unidades) {		
+		
+		return (unidades >= 1)?true:false;
+		
+	}
+	
+	public static boolean validarString(String cadena) {
+		boolean validacionOk;
+
+		//return (!cadena.isEmpty());
+		if(!cadena.isBlank()) {
+			validacionOk=true;
+		}else {
+			validacionOk=false;
+		}		
+		return validacionOk;
+	}
+	
+	public static boolean validarString(String cadena, int longitudMax) {
+		boolean validacionOk;
+
+		//return (!cadena.isEmpty());
+		if(!cadena.isBlank() && cadena.length() <= longitudMax) {
+			validacionOk=true;
+		}else {
+			validacionOk=false;
+		}		
+		return validacionOk;
+	}
+	
+	public static Categorias chkCategorias(String entCat) {
+		Categorias cat=null;
+		char letra = ' ';
+		
+		if(entCat.length() == 1) {
+			letra = entCat.charAt(0);
+		}
+		
+		if(entCat.equals(Categorias.CATA.getDesc().toUpperCase().replace("�", "I"))
+				|| letra == '1' || letra == 'A') {
+				cat=Categorias.CATA;
+			}else if(entCat.equals(Categorias.CATB.getDesc().toUpperCase().replace("�", "I"))
+					|| letra == '2' || letra == 'B'){
+				cat = Categorias.CATB;
+			}else if(entCat.equals(Categorias.CATC.getDesc().toUpperCase().replace("�", "I"))
+					|| letra == '3' || letra == 'C') {
+				cat = Categorias.CATC;
+			}
+		
+		return cat;
+	}
+	
+}
