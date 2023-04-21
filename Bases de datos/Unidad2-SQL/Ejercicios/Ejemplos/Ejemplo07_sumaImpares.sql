3160f244b1b81a120df42c3d7bf0a5bd2838df28
Qv1ko <vicgarmur947@gmail.com>
2023-04-21 13:01:19 +0200

    Correción ejercicio de suma de impares

--- Bases de datos/Unidad2-SQL/Ejercicios/Ejemplos/Ejemplo07_sumaImpares.sql
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejemplos/Ejemplo07_sumaImpares.sql
@@ -1,9 +1,11 @@
 DELIMITER //
 CREATE FUNCTION sumImpares(n INT) RETURNS INT
 BEGIN
-    DECLARE result INT DEFAULT 1;
+    DECLARE result INT DEFAULT 0;
+    DECLARE impar INT DEFAULT 1;
     WHILE n>0 DO
-        SET result=result+2;
+        SET result=result+impar;
+        SET impar=impar+2;
         SET n=n-1;
     END WHILE;
     RETURN result;
