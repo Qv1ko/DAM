a88f52f786a182d476e5e79bbac9498579fa792c
Qv1ko <vicgarmur947@gmail.com>
2023-04-25 10:58:31 +0200

    Clase de herencia y ficheros

---
+++ Programación/Java/src/paqueteClases3/Clase12_Herencia/Nas/Libros.java
@@ -0,0 +1,33 @@
+package paqueteClases3.Clase12_Herencia.Nas;
+
+public class Libros extends Multimedia {
+
+    private String autor;
+
+    public Libros() {
+        super();
+        this.autor = "desconocido";
+    }
+
+    public Libros(String titulo, String genero, String autor) {
+        super(titulo, genero);
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String toString() {
+        return "El libro "+super.toString()+" es del autor "+getAutor();
+    }
+
+    public String toFile() {
+        return getTitulo()+";"+getGenero()+";"+getAutor();
+    }
+
+}
---
+++ Programación/Java/src/paqueteClases3/Clase12_Herencia/Nas/Principal.java
@@ -0,0 +1,48 @@
+package paqueteClases3.Clase12_Herencia.Nas;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // Peliculas peli = new Peliculas("Peter pan", "Infantil", 90);
+        // Series serie = new Series("Naruto", "Anime", 17);
+        // Libros libro = new Libros("El cid Campeador", "Acción", "Miguel de Cervantes");
+        // ArrayList<Multimedia> lista = new ArrayList<Multimedia>();
+        // lista.add(peli);
+        // lista.add(serie);
+        // lista.add(libro);
+        // for(Multimedia m:lista) {
+        //     System.out.println(m);
+        // }
+        BufferedReader br = null;
+        ArrayList<Multimedia> lista = new ArrayList<Multimedia>();
+        String[] linea = null;
+        try {
+            br = new BufferedReader(new FileReader("Programación\\Java\\src\\paqueteClases3\\Clase12_Herencia\\Nas\\media.csv"));
+            while (br.ready()) {
+                linea = br.readLine().split(";");
+                try {
+                    if (linea[0].equalsIgnoreCase("P")) {
+                        lista.add(new Peliculas(linea[1], linea[2], Integer.parseInt(linea[3])));
+                    } else if (linea[0].equalsIgnoreCase("S")) {
+                        lista.add(new Series(linea[1], linea[2], Integer.parseInt(linea[3])));
+                    } else if (linea[0].equalsIgnoreCase("L")) {
+                        lista.add(new Libros(linea[1], linea[2], linea[3]));
+                    }
+                } catch (NumberFormatException exc) {
+                    System.out.println("\n! Dato no númerico "+linea[3]+" en "+linea[1]+"\n");
+                }
+            }
+        } catch (FileNotFoundException exc) {
+            System.out.println("\n! Fichero no existe\n");
+        } catch (IOException exc) {
+            System.out.println("\n! Error E/S\n");
+        }
+    }
+
+}
