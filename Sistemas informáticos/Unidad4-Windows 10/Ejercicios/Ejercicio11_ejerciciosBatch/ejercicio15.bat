cba211c98b1e92c641abb9d43ec1dcd5510ec9f4
Qv1ko <vicgarmur947@gmail.com>
2023-04-25 13:07:14 +0200

    Correción ejercicios batch

--- Sistemas informáticos/Unidad4-Windows 10/Ejercicios/Ejercicio11_ejerciciosBatch/ejercicio15.bat
+++ Sistemas informáticos/Unidad4-Windows 10/Ejercicios/Ejercicio11_ejerciciosBatch/ejercicio15.bat
@@ -1,12 +1,15 @@
 @ECHO OFF
 
 CLS
-GOTO :loop
-
-:error
-ECHO Tecla incorrecta
-
+ECHO Pulsa la tecla x para salir
 :loop
-ECHO Pulse la tecla x
-CHOICE /C x > nul
-IF errorlevel 2 GOTO :error
+rem Almacena la tecla pulsada en una variable
+SET /P tecla= Introduce una tecla: 
+rem Condicion si la variable es igual a x sale, si no repite el :loop
+IF "%tecla%"=="x" (
+	ECHO Saliendo...
+	rem Sale del archivo por lotes sin cerrar la terminal
+	EXIT /B
+) ELSE (
+	GOTO loop
+)
--- Sistemas informáticos/Unidad4-Windows 10/Ejercicios/Ejercicio11_ejerciciosBatch/ejercicio16_ayuda.bat
+++ Sistemas informáticos/Unidad4-Windows 10/Ejercicios/Ejercicio11_ejerciciosBatch/ejercicio16_ayuda.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 
+rem Condicion si el primer parametro introducido es vacio muestra un mensaje de uso, si no muestra la ayuda del parametro
 IF "%1"=="" (
 	ECHO Uso: ayuda comando
 ) ELSE (
 	%1 /?
 	ECHO Pulse una tecla para salir...
-	PAUSE > nul
 )
+PAUSE > nul
