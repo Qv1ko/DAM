d188dc7ce8b52a1f22449a1ece88f0adb3bd25b8
Qv1ko <vicgarmur947@gmail.com>
2023-05-10 10:12:18 +0200

    Metodo de añadir nave y ficheros

--- Programación/Java/src/unidad6/ejercicios/ejercicio03_Hangar/Hangar.java
+++ Programación/Java/src/unidad6/ejercicios/ejercicio03_Hangar/Hangar.java
@@ -1,6 +1,9 @@
 package unidad6.ejercicios.ejercicio03_Hangar;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -10,19 +13,29 @@ public class Hangar {
 
     public static void main(String[] args) {
         // Variables y objetos
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)), br = null;
+        BufferedWriter bw = null;
         int opcion = 0;
         Map<String, Naves> navesHangar = new HashMap<String, Naves>();
+        // Importar datos
+        try {
+            br = new BufferedReader(new FileReader("Programación\\Java\\src\\unidad6\\ejercicios\\ejercicio03_Hangar\\Data.csv"));
+            if(br.ready()) {
+                br.readLine().split(";");
+            }
+        } catch(IOException exc) {
+            System.out.println("\n! Error de entrada o salida\n");
+        }
         // Menu de opciones
         do {
             try {
-                System.out.print("\nMenu de opciones:\n1.) Añadir nave\n2.) Eliminar nave\n3.) Ver naves\n4.) Salir\nSelecciona una opción: ");
+                System.out.print("\nMenu de opciones:\n1.) Añadir nave\n2.) Eliminar nave\n3.) Ver naves\n0.) Salir\nSelecciona una opción: ");
                 opcion = Integer.parseInt(bf.readLine());
                 switch (opcion) {
                     case 1 -> addNave(bf, navesHangar);
                     case 2 -> delNave(bf, navesHangar);
                     case 3 -> listNaves(navesHangar);
-                    case 4 -> System.out.println("\n! Saliendo...\n");
+                    case 0 -> System.out.println("\n! Saliendo...\n");
                     default -> System.out.println("\n! Opción no valida\n");
                 }
             } catch (IOException exc) {
@@ -32,74 +45,63 @@ public class Hangar {
             } catch (NumberFormatException exc) {
                 System.out.println("\n! Opcion no valida\n");
             }
-        } while (opcion!=4);
+        } while (opcion != 0);
+        // Guardar naves
+        try {
+            bw = new BufferedWriter(new FileWriter("Programación\\Java\\src\\unidad6\\ejercicios\\ejercicio03_Hangar\\Data.csv"));
+            for (String naveKey : navesHangar.keySet()) {
+                bw.write(navesHangar.get(naveKey).toFile());
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException exc) {
+            System.out.println("\n! Error de entrada o salida\n");
+        }
     }
 
     private static void addNave(BufferedReader bf, Map<String, Naves> navesHangar) {
-        String tipoNave = "", opcion = "", bando = "";
-        int pasos = 0;
-        Ties tie = null;
-        Wings wing = null;
-        do {
+        String[] datos = new String[3];
+        boolean run = true;
+        System.out.println("Introduce los datos de la nave:");
+        while (run) {
             try {
-                if(pasos<1) {
-                    System.out.print("Escribe el nombre del tipo de nave ha añadir: ");
-                    tipoNave = bf.readLine();
-                    pasos++;
-                }
-                if(tipoNave.equalsIgnoreCase("tie")) {
-                    tie = new Ties();
-                    System.out.println("Introduce los datos del TIE:");
-                    if(pasos<2) {
-                        System.out.print("Matricula: ");
-                        tie.setMatricula(bf.readLine());
-                        pasos++;
-                    }
-                    if(pasos<3) {
-                        System.out.print("Bando: ");
-                        bando = bf.readLine();
-                        tie.setImperio((bando.equalsIgnoreCase("IMP") || bando.equalsIgnoreCase("IMPERIO"))? true : false);
-                        pasos++;
-                    }
-                    if(pasos<4) {
-                        System.out.print("Cilindradas: ");
-                        tie.setCilindrada(Integer.parseInt(bf.readLine()));
-                        pasos++;
-                    }
-                    navesHangar.put(tie.getMatricula(), tie);
-                } else if(tipoNave.equalsIgnoreCase("wing")) {
-                    wing = new Wings();
-                    System.out.println("Introduce los datos del WING:");
-                    if(pasos<2) {
-                        System.out.print("Matricula: ");
-                        wing.setMatricula(bf.readLine());
-                        pasos++;
-                    }
-                    if(pasos<3) {
-                        System.out.print("Bando: ");
-                        bando = bf.readLine();
-                        wing.setImperio((bando.equalsIgnoreCase("IMP") || bando.equalsIgnoreCase("IMPERIO"))? true : false);
-                        pasos++;
-                    }
-                    if(pasos<4) {
-                        System.out.print("Número de motores: ");
-                        wing.setMotores(Integer.parseInt(bf.readLine()));
-                        pasos++;
-                    }
-                    navesHangar.put(wing.getMatricula(), wing);
-                } else {
-                    System.out.println("\n! Tipo de nave invalido\n");
-                    pasos=0;
-                    System.out.print("Quieres salir? (S/N): ");
-                    opcion= bf.readLine();
-                    if(opcion.equalsIgnoreCase("S") || opcion.equalsIgnoreCase("SI")) {
-                        break;
-                    }
-                }
+                System.out.print("Matricula: ");
+                datos[0] = bf.readLine();
+                run = false;
+            } catch (IOException exc) {
+                System.out.println("\n! Error al introducir la matricula\n");
+            }
+        }
+        run = true;
+        while (run) {
+            try {
+                System.out.print("Pertenece al imperio (S/N): ");
+                datos[1] = bf.readLine().toLowerCase();
+                run = !(datos[1].charAt(0) == 's' || datos[1].charAt(0) == 'n');
             } catch (IOException exc) {
                 System.out.println("\n! Error de entrada o salida\n");
             }
-        } while (pasos<4);
+        }
+        run = true;
+        while (run) {
+            try {
+                System.out.print("Tipo de nave (Wing/Tie): ");
+                switch(bf.readLine().toLowerCase().charAt(0)) {
+                    case 't':
+                        System.out.print("Numero de cilindradas: ");
+                        datos[2] = bf.readLine();
+                        run = false;
+                        break;
+                    case 'w':
+                        System.out.print("Numero de motores: ");
+                        datos[2] = bf.readLine();
+                        run = false;
+                        break;
+                }
+            } catch (IOException exc) {
+                System.out.println("\n! Error al elegir el tipo de nave\n");
+            }
+        }
     }
 
     private static void delNave(BufferedReader bf, Map<String, Naves> navesHangar) throws IOException, NullPointerException {
@@ -110,7 +112,7 @@ public class Hangar {
 
     private static void listNaves(Map<String, Naves> navesHangar) {
         System.out.println("\nListado de naves del hangar:\n");
-        for(String naveKey:navesHangar.keySet()) {
+        for (String naveKey : navesHangar.keySet()) {
             System.out.println(navesHangar.get(naveKey).toString());
         }
     }
