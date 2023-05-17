ef4409d03b8e809d0b5593f27eabc3bf60dcecbc
Qv1ko <vicgarmur947@gmail.com>
2023-05-18 01:35:42 +0200

    Base de datos y tablas del repaso

--- Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio21/Ejercicio21.sql
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio21/Ejercicio21.sql
@@ -1,5 +1,28 @@
-Crear base de datos "Mayo"
-Tabla Comercial(id,nombre,comision DEC(6,2))
-Tabla pedidos(id,importe DEC(5,1),fecha,id_cliente,id_comercial)
-Tabla clientes(id_cliente,apellido,debe DEC(7,1))
-Relacionadas
+-- 1. Crea la base de datos Mayo
+CREATE DATABASE mayo;
+use mayo;
+
+-- 2. Crea la tabla comercial(id, nombre, comision DEC(6,2))
+CREATE TABLE comercial(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(20),
+    comision DEC(6,2)
+);
+
+-- 3. Crea la tabla clientes(id, apellido, debe DEC(7,1))
+CREATE TABLE clientes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    apellido VARCHAR(30),
+    debe DEC(7,1)
+);
+
+-- 4. Crea la tabla pedidos(id, importe DEC(5,1), fecha, id_comercial, id_cliente)
+CREATE TABLE pedidos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    importe DEC(5,1),
+    fecha DATE,
+    id_comercial INT,
+    id_cliente INT,
+    FOREIGN KEY(id_comercial) REFERENCES comercial(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(id_cliente) REFERENCES clientes(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
