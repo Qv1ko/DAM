a84cd185cc0bf4fe88a697cdd38e96c64961b142
Qv1ko <vicgarmur947@gmail.com>
2023-05-17 13:52:55 +0200

    Ejercicio repaso

---
+++ Bases de datos/Unidad2-SQL/Ejercicios/Ejercicio21/Ejercicio21.sql
@@ -0,0 +1,5 @@
+Crear base de datos "Mayo"
+Tabla Comercial(id,nombre,comision DEC(6,2))
+Tabla pedidos(id,importe DEC(5,1),fecha,id_cliente,id_comercial)
+Tabla clientes(id_cliente,apellido,debe DEC(7,1))
+Relacionadas
