4bc0514725f13e700d7f4dfda5de92a653f8927b
Qv1ko <vicgarmur947@gmail.com>
2023-05-18 10:18:41 +0200

    Ejercicios batch

--- Sistemas informáticos/Unidad4-Windows 10/Examen/ejercicio3.bat
+++ Sistemas informáticos/Unidad4-Windows 10/Examen/ejercicio3.bat
@@ -1,5 +1,17 @@
 @ECHO OFF
-DATE /T
-TIME /TIME
-VER
-TASKLIST
+IF "%1"=="" (
+    ECHO ERRPR Batch sin parametros.
+    ECHO Debe introducir el nombre del fichero en la orden.
+    GOTO fin
+)
+IF NOT EXIST %USERPROFILE%\desktop\informes (
+    ECHO Directorio <informe> no existe
+    MD %USERPROFILE%\desktop\informes
+    ECHO Directorio <informes> creado
+)
+DATE /T > %1
+TIME /T >> %1
+VER >> %1
+TASKLIST >> %1
+:fin
+ECHO Finalizando archivo Batch
