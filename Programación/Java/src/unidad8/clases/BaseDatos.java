64407e1215b5a528ed946bf43c8a677da727b2a7
Qv1ko <vicgarmur947@gmail.com>
2023-05-22 13:11:52 +0200

    Configuración de la conexión a la base de datos

--- Programación/Java/src/unidad8/clases/BaseDatos.java
+++ Programación/Java/src/unidad8/clases/BaseDatos.java
@@ -3,68 +3,62 @@ package unidad8.clases;
 import java.sql.*;
 
 public class BaseDatos {
-	
-	//Rellenar con los datos de la conexi�n a la BBDD
-	private String bd = "base";	
-	private String url="jdbc:mysql://localhost:3306/" + bd + "?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	private String usuario = "manu";
-	private String password = "mipass";
-	
-	//Objetos propios del paquete sql
-	private Connection conn = null; // Objeto para la conexi�n
+
+	// Rellenar con los datos de la conexión a la BBDD
+	private String bd = "base";
+	private String url = "jdbc:mysql://localhost:3306/" + bd + "?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private String usuario = "root";
+	private String password = "";
+
+	// Objetos propios del paquete sql
+	private Connection conn = null; // Objeto para la conexion
 	private Statement stmt = null; // Objeto para realizar sentencias SQL
 	private ResultSet rs = null; // Objeto donde se almacenan los registros devueltos de una SELECT
-	
-		 
+
 	public BaseDatos() {
-		try{
-			//Cargamos el driver de la conexi�n
+		try {
+			// Cargamos el driver de la conexion
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			//Creamos la conexi�n
+			// Creamos la conexion
 			conn = DriverManager.getConnection(url, usuario, password);
-		}catch (Exception e){
-			//Mostramos error si no se puede conectar (error puerto, nombre, pass ...)
-			System.out.println("Error de conexi�n: " + e.getMessage());
+		} catch (Exception e) {
+			// Mostramos error si no se puede conectar (error puerto, nombre, pass ...)
+			System.out.println("Error de conexión: " + e.getMessage());
 		}
 	}
-	
+
 	public void modificarDatos(String query) {
-		
-		//Metodo creado para las operaciones de INSERT, UPDATE o DELETE
-		
+		// Metodo creado para las operaciones de INSERT, UPDATE o DELETE
 		try {
 			stmt = conn.prepareStatement(query);
 		} catch (SQLException e) {
-			System.out.println("Error en insertarDatos: "+e.getMessage());
+			System.out.println("Error en insertarDatos: " + e.getMessage());
 		}
-		
-		//Varable entera que almacena el numero de registros insertados, actualizados o eliminados
-		int i=0;
+		// Variable entera que almacena el numero de registros insertados, actualizados o eliminados
+		int i = 0;
 		try {
-			//Aplicamos la sentencia SQL
+			// Aplicamos la sentencia SQL
 			i = stmt.executeUpdate(query);
 		} catch (SQLException e) {
-			System.out.println("Error SQL: "+query+"\n"+e.getMessage());			
+			System.out.println("Error SQL: " + query + "\n" + e.getMessage());
 		}
 		System.out.println((i + " Filas actualizadas"));
 	}
-		
-	public ResultSet consultaBD(String query) {	
-	
-		//Metodo para almacenar en un objeto ResultSet la sentencia SELECT realizada
-		try{
+
+	public ResultSet consultaBD(String query) {
+		// Metodo para almacenar en un objeto ResultSet la sentencia SELECT realizada
+		try {
 			stmt = conn.createStatement();
-			rs = stmt.executeQuery(query);			
-		}catch(Exception e){
-			System.out.println("Error consulta: "+e.getMessage());
+			rs = stmt.executeQuery(query);
+		} catch (Exception e) {
+			System.out.println("Error consulta: " + e.getMessage());
 		}
 		return rs;
 	}
-	
-	public void cerrarCons() {
 
+	public void cerrarCons() {
 		try {
-			if(rs != null){
+			if (rs != null) {
 				rs.close();
 				stmt.close();
 			}
@@ -73,14 +67,13 @@ public class BaseDatos {
 		}
 		cerrarBD();
 	}
-	
+
 	public void cerrarBD() {
-		try{
+		try {
 			conn.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("Error conn.close()");
 		}
 	}
 
-	
 }
--- Programación/Java/src/unidad8/clases/PruebaBBDD.java
+++ Programación/Java/src/unidad8/clases/PruebaBBDD.java
@@ -4,51 +4,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PruebaBBDD {
-	
+
 	public static BaseDatos bd;
-	
-	public static void main(String[] args)  {
-		
-		//Creamos el objeto que nos conecta a la BBDD
+
+	public static void main(String[] args) {
+		// Creamos el objeto que nos conecta a la BBDD
 		BaseDatos bbdd = new BaseDatos();
-		
-		
-		//Diferentes sentencias SQL
-//		String query = "INSERT INTO Productos (Nombre, descripcion, precio) VALUES ('peras','muy ricas','2')";		
-//		String query = "UPDATE productos SET precio='3' WHERE Nombre='peras'";
-//		String query = "DELETE FROM productos WHERE Nombre='peras'";
-		
-		
-		//Metodo para realizar las operaciones anteriores de INSERT, UPDATE o DELETE
-//		bbdd.modificarDatos(query);
-		
-		
-		//Sentencia para seleccionar registros
+
+		// Diferentes sentencias SQL
+		// String query = "INSERT INTO Productos (Nombre, descripcion, precio) VALUES ('peras','muy ricas','2')";
+		// String query = "UPDATE productos SET precio='3' WHERE Nombre='peras'";
+		// String query = "DELETE FROM productos WHERE Nombre='peras'";
+
+		// Metodo para realizar las operaciones anteriores de INSERT, UPDATE o DELETE
+		// bbdd.modificarDatos(query);
+
+		// Sentencia para seleccionar registros
 		String select = "SELECT * FROM vendedores";
-		
-		//El metodo devuelve un ResultSet (imaginad que es un array o ArrayList)
+
+		// El metodo devuelve un ResultSet (imaginad que es un array o ArrayList)
 		ResultSet rs = bbdd.consultaBD(select);
-		
+
 		try {
-			//Mientras existan registros lee. 						
-			while(rs.next()) {
-				//Podemos acceder a los registros de la select mediante el nombre de la columna de la tabla
-				System.out.println("nombre :"+ rs.getString("nombre"));
-				System.out.println("descripcion :"+ rs.getString("dni"));
-				System.out.println("precio :"+ rs.getInt("telefono"));
-				
+			// Mientras existan registros lee.
+			while (rs.next()) {
+				// Podemos acceder a los registros de la select mediante el nombre de la columna de la tabla
+				System.out.println("nombre :" + rs.getString("nombre"));
+				System.out.println("descripcion :" + rs.getString("dni"));
+				System.out.println("precio :" + rs.getInt("telefono"));
 				// O bien acceder al indice de la columna de la tabla
-				System.out.println("nombre :"+ rs.getString(1));
-				System.out.println("dni :"+ rs.getString(2));
-				System.out.println("telefono :"+ rs.getInt(3));
-			
-			//IMPORTANTE QUE EL GET DEL rs coincida con el tipo de campo de la tabla
-			
+				System.out.println("nombre :" + rs.getString(1));
+				System.out.println("dni :" + rs.getString(2));
+				System.out.println("telefono :" + rs.getInt(3));
+				// IMPORTANTE QUE EL GET DEL rs coincida con el tipo de campo de la tabla
 			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-		//cerramos la conexion
+		// Cerramos la conexion
 		bbdd.cerrarCons();
 	}
 
