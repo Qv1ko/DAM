41bf621a86186830a17db825a1136dbdf965bb55
Qv1ko <vicgarmur947@gmail.com>
2023-08-09 20:48:39 +0200

    Interfaces graficas 1

--- Programación/Java/src/Verano/Interfaces/Interfaces1.java
+++ Programación/Java/src/Verano/Interfaces/Interfaces1.java
@@ -1,5 +1,31 @@
 package Verano.Interfaces;
 
+import javax.swing.*;
+
 public class Interfaces1 {
+    
+
+    public static void main(String[] args) {
+        
+        window w = new window();        
+
+    }
+
+}
+
+class window extends JFrame {
+
+    public window(){
+
+        // Dimensiones de la ventana
+        setSize(500, 300);
+
+        // Hacer la ventana visible
+        setVisible(true);
+
+        // Funcion para que al cerrar la ventana el programa finalice
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
     
 }
