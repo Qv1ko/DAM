e12aaf71991443831c3f3b110f8d2709ac0a54cd
Qv1ko <vicgarmur947@gmail.com>
2023-08-10 22:47:21 +0200

    Intefaces graficas 2

--- Programación/Java/src/Verano/Interfaces/Interfaces1.java
+++ Programación/Java/src/Verano/Interfaces/Interfaces1.java
@@ -1,5 +1,7 @@
 package Verano.Interfaces;
 
+import java.awt.Frame;
+
 import javax.swing.*;
 
 public class Interfaces1 {
@@ -18,13 +20,28 @@ class window extends JFrame {
     public window(){
 
         // Dimensiones de la ventana
-        setSize(500, 300);
+        // setSize(500, 300);
 
         // Hacer la ventana visible
         setVisible(true);
 
         // Funcion para que al cerrar la ventana el programa finalice
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Localizacion de la ventana en un eje de cordenadas (pantalla)
+        // setLocation(500,300);
+
+        // Dimensiones y localizacion de la ventana
+        setBounds(500, 300, 550, 250);
+
+        // Modificacion de las dimensiones de la ventana
+        // setResizable(false);
+
+        // Permite maximizar la ventana
+        // setExtendedState(Frame.MAXIMIZED_BOTH);
+
+        // Titulo de la ventana
+        setTitle("Graphic window");
 
     }
     
