004f5a2000ba47ed81310b16c04f124b0f6f6ae5
Qv1ko <vicgarmur947@gmail.com>
2023-08-16 14:47:04 +0200

    Interfaces graficas 8

---
+++ Programación/Java/src/Verano/Interfaces/InterfazGrafica6.java
@@ -0,0 +1,41 @@
+package Verano.Interfaces;
+
+import java.awt.GraphicsEnvironment;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InterfazGrafica6 {
+    
+    public static void main(String[] args) {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String font = "";
+        boolean exist = false;
+
+        System.out.print("Fuente: ");
+        try {
+            font = br.readLine();
+        } catch (IOException exc) {
+            System.out.println(exc.getMessage());
+        }
+
+        String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
+        for (String name : fontNames) {
+            
+            if (font.equalsIgnoreCase(name)) {
+                exist = true;
+            }
+
+        }
+
+        if (exist) {
+            System.out.println("Fuente instalada");
+        } else {
+            System.out.println("No esta instalada la fuente");
+        }
+
+    }
+
+}
---
+++ Programación/Java/src/Verano/Interfaces/InterfazGrafica7.java
@@ -0,0 +1,62 @@
+package Verano.Interfaces;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+public class InterfazGrafica7 {
+    
+    public static void main(String[] args) {
+        
+        FontsWindow window = new FontsWindow();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setVisible(true);
+
+    }
+
+}
+
+class FontsWindow extends JFrame {
+
+    public FontsWindow() {
+
+        setTitle("Fonts Window");
+
+        setSize(600, 450);
+
+        FontLayer layer1 = new FontLayer();
+        add(layer1);
+
+        // Color para todos los graficos de la ventana
+        layer1.setForeground(Color.BLUE);
+
+    }
+
+}
+
+class FontLayer extends JPanel {
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Fuente
+        Font myFont = new Font("Arial", Font.BOLD, 15);
+        g2d.setFont(myFont);
+
+        // g2d.setColor(Color.ORANGE);
+
+        // Texto
+        g2d.drawString("Lorem ipsum dolor sit amet.", 100, 100);
+
+        g2d.setFont(new Font("Verdana", Font.ITALIC, 20));
+        // g2d.setColor(Color.BLACK);
+        g2d.drawString("Lorem ipsum dolor sit amet.", 100, 200);
+
+    }
+
+}
