8fcb72ee930d874e9cffc0a8a88d82a76b659b76
Qv1ko <vicgarmur947@gmail.com>
2023-09-25 10:15:18 +0200

    Controlador

---
+++ Programación multimedia y dispositivos móviles/Controller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+using TMPro;
+using UnityEngine.UI;
+
+// : -> extends
+public class Controller : MonoBehaviour {
+
+    public Image img;
+    public TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        text.text = "Hello world!";
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+}
