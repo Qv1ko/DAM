57462010858e63288824d3eb385b287340eebb47
Qv1ko <vicgarmur947@gmail.com>
2023-10-05 12:27:39 +0200

    Clase carácteres de escape

--- Programación de servicios y procesos/Java/src/unidad1/clases/Clase03_CaracteresEscape.java
+++ Programación de servicios y procesos/Java/src/unidad1/clases/Clase03_CaracteresEscape.java
@@ -2,6 +2,6 @@ package unidad1.clases;
 
 public class Clase03_CaracteresEscape {
 
-    String path = "C:\\Users\\Usuario\\Desktop";
+    String path = "C:\\Users\\Usuario\\Desktop"; // Se usan caracteres de escape en las rutas, para escapar la contrabarra
     
 }
