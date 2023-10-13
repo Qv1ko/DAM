f0ba396e31b143df79ed92eae4c780bb63742185
Qv1ko <vicgarmur947@gmail.com>
2023-10-13 16:14:00 +0200

    Story board finalizado

--- Programación multimedia y dispositivos móviles/Ejercicios/Ejercicio00_StoryBoard/StoryBoard.cs
+++ Programación multimedia y dispositivos móviles/Ejercicios/Ejercicio00_StoryBoard/StoryBoard.cs
@@ -11,43 +11,44 @@ public class StoryBoard : MonoBehaviour {
 
     public List<Content> slides = new();
 
-    private int lastSlide = 0, slideIndex = 0;
-    private bool save = true;
+    private int slideIndex = 0;
 
     void Start() {
 
         slides.Add(new Content("Me levanto de la cama", "slide1", "[==========]", false));
-        slides.Add(new Content("Enciendo la televisiÛn", "slide2", "<- Coger comida | Tumbarme en el sof· ->", false));
+        slides.Add(new Content("Enciendo la televisi√≥n", "slide2", "<- Coger comida | Tumbarme en el sof√° ->", false));
         slides.Add(new Content("Voy a buscar algo de comer", "slide3", "[==========]", true));
-        slides.Add(new Content("Me tumbo en el sof·", "slide4", "[==========]", false));
-        slides.Add(new Content("Pongo DAZN en la televisiÛn", "slide5", "[==========]", false));
-        slides.Add(new Content("Comienza la carrera de FÛrmula 1", "slide6", "<- Dormir | Continuar viendo la carrera ->", false));
-        slides.Add(new Content("Me quedo dormido en el sof·", "slide7", "[==========]", true));
-        slides.Add(new Content("En la carrera, la esuderÌa de mi piloto favorito comete errores graves", "slide8", "[==========]", false));
+        slides.Add(new Content("Me tumbo en el sof√°", "slide4", "[==========]", false));
+        slides.Add(new Content("Pongo DAZN en la televisi√≥n", "slide5", "[==========]", false));
+        slides.Add(new Content("Comienza la carrera de F√≥rmula 1", "slide6", "<- Dormir | Continuar viendo la carrera ->", false));
+        slides.Add(new Content("Me quedo dormido en el sof√°", "slide7", "[==========]", true));
+        slides.Add(new Content("En la carrera, la esuder√≠a de mi piloto favorito comete errores graves", "slide8", "[==========]", false));
         slides.Add(new Content("La carrera termina con el tedioso resultado de siempre", "slide9", "[==========]", false));
-        slides.Add(new Content("14 de noviembre de 2010", "slide10", "Todo fue un sueÒo...", false));
+        slides.Add(new Content("14 de noviembre de 2010", "slide10", "Todo fue un sue√±o...", false));
 
         DisplaySlide(slideIndex);
 
     }
     void Update() {
 
-        if (slides[slideIndex + 1].isFirstCodition) {
-            //if (UnityEngine.Input.GetKeyDown(KeyCode.Backspace)) {
-            //    slideIndex = lastSlide;
-            //} else if (UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow)) {
-            //    slideIndex++;
-            //} else if (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow)) {
-            //    slideIndex += 2;
-            //}
-            slideIndex = (UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow)) ? slideIndex + 1 : (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow)) ? slideIndex + 2 : UnityEngine.Input.GetKeyDown(KeyCode.Backspace) ? slideIndex = lastSlide : 0;
-        } else if (!slides[slideIndex + 1].isFirstCodition) {
-            slideIndex = (UnityEngine.Input.GetKeyDown(KeyCode.Space) && slides[slideIndex].isFirstCodition) ? slideIndex + 2 : (UnityEngine.Input.GetKeyDown(KeyCode.Space)) ? slideIndex + 1 : lastSlide;
-            //if (UnityEngine.Input.GetKeyDown(KeyCode.Backspace)) {
-            //    slideIndex = lastSlide;
-            //} else if (UnityEngine.Input.GetKeyDown(KeyCode.Space)) {
-            //    slideIndex++;
-            //}
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (slideIndex < slides.Count && !slides[slideIndex + 1].isFirstCodition) {
+                slideIndex += (slides[slideIndex].isFirstCodition) ? 2 : 1;
+            }
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            if (slideIndex < slides.Count && slides[slideIndex + 1].isFirstCodition) {
+                slideIndex++;
+            }
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (slideIndex < slides.Count && slides[slideIndex + 1].isFirstCodition) {
+                slideIndex += 2;
+            }
+        } else if (Input.GetKeyDown(KeyCode.Backspace)) {
+            if (slideIndex > 1) {
+                slideIndex -= (slides[slideIndex - 1].isFirstCodition) ? 2 : (slides[slideIndex - 2].isFirstCodition) ? 3 : 1;
+            } else if (slideIndex > 0) {
+                slideIndex -= (slides[slideIndex - 1].isFirstCodition) ? 2 : 1;
+            }
         }
 
         DisplaySlide(slideIndex);
@@ -56,20 +57,11 @@ public class StoryBoard : MonoBehaviour {
 
     void DisplaySlide(int slideIndex) {
 
-        slideText.text = slides[slideIndex].primaryText;
-        slideImage.sprite = slides[slideIndex].img;
-        slideAction.text = slides[slideIndex].secondaryText;
-
-        //if (slides[slideIndex + 1].isFirstCodition && save) {
-        //    lastSlide = (slideIndex > 0) ? slideIndex - 1 : 0;
-        //    save = false;
-        //} else if (save) {
-        //    lastSlide = (slideIndex > 0) ? (slides[slideIndex].isSecondCodition) ? slideIndex - 2 : slideIndex - 1 : 0;
-        //} else {
-        //    save = true;
-        //}
-
-
+        if (slideIndex >= 0 && slideIndex < slides.Count) {
+            slideText.text = slides[slideIndex].primaryText;
+            slideImage.sprite = slides[slideIndex].img;
+            slideAction.text = slides[slideIndex].secondaryText;
+        }
 
     }
 
