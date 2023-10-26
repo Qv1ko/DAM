c00546f51c37edda85e6fdd1efe38aa0f83c5330
Qv1ko <vicgarmur947@gmail.com>
2023-10-26 10:27:41 +0200

    Creación de tablas

--- Trabajo de fin de grado/alquiler_portatiles.sql
+++ Trabajo de fin de grado/alquiler_portatiles.sql
@@ -5,4 +5,39 @@ CREATE DATABASE alquiler_portatiles;
 
 USE alquiler_portatiles;
 
-/* crear tablas */
+/* creación de las tablas */
+
+-- alumnos
+
+CREATE OR REPLACE TABLE alumnos(
+  id_alumno varchar(8) AUTO_INCREMENT,
+  dni varchar(8),
+  nombre varchar(20),
+  apellidos varchar(48),
+  curso varchar(96),
+  aula varchar(4),
+  turno varchar(8),
+  -- estado_matricula enum(),
+  -- foreign keys
+  id_portatil varchar(8),
+  id_cargador varchar(8),
+  id_raton varchar(8),
+  PRIMARY KEY (id_alumno)
+);
+
+CREATE OR REPLACE TABLE portatiles(
+  id_portatil varchar(8),
+  codigo varchar(8),
+  marca varchar(20),
+  modelo varchar(16),
+  procesador varchar(20),
+  memoria_ram varchar(16),
+  dispositivo_almacenamiento varchar(20),
+  capacidad_almacenamiento int(20),
+  estado_alquiler varchar(20),
+  -- foreign keys
+  id_cargador varchar(8),
+  id_raton varchar(8),
+  id_almacen varchar(8),
+  PRIMARY KEY (id_portatil)
+);
