0470e7053eeda0189dfb2db09970453cb84d576e
Qv1ko <vicgarmur947@gmail.com>
2023-11-08 11:41:34 +0100

    Ejercicio 6 - Asociación

---
+++ Programación de servicios y procesos/Java/src/unidad1/ejercicios/ejercicio06_Asociacion/Flow.java
@@ -0,0 +1,71 @@
+package unidad1.ejercicios.ejercicio06_Asociacion;
+
+public class Flow {
+  
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+}
+
+class Recinto {
+
+    private boolean intruso;
+
+    public Recinto() {
+        this.intruso = false;
+    }
+
+    public boolean getIntruso() {
+        return this.intruso;
+    }
+
+    public void setIntruso(boolean intruso) {
+        this.intruso = intruso;
+    }
+
+}
+
+class Sensor extends Recinto {
+
+    private boolean activo;
+
+    public Sensor() {
+        super();
+        this.activo = false;
+    }
+
+    public boolean getActivo() {
+        return this.activo;
+    }
+
+    public void comprobarActivo(boolean intruso) {
+        this.activo = intruso;
+    }
+
+}
+
+class Actuador extends Sensor {
+
+    private boolean disparador;
+
+    public Actuador() {
+        super();
+        this.disparador = false;
+    }
+
+    public boolean getDisparador() {
+        return this.disparador;
+    }
+
+    public void comprobarDisparador(boolean activo) {
+        this.disparador = activo;
+    }
+
+    public void alarma() {
+        System.out.println(getDisparador() ? "ALERTA INTRUSO" : "");
+    }
+
+}
