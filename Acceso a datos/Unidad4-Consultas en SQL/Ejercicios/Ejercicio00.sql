908ce0c0d72c00f5b246953f52f314e73d03e17c
Qv1ko <vicgarmur947@gmail.com>
2023-11-10 19:06:44 +0100

    Ejercicio 0

--- Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio00.sql
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio00.sql
@@ -1,4 +1,4 @@
-﻿-- Encontra la provincia menos poblada de la autonomia mas poblada
+﻿-- Encontrar la provincia menos poblada de la autonomia mas poblada
 
 -- autonomiaĢautonomia, SUM(poblacion) as pobauto (provincias) -> c1
 -- ĢMAX(pobauto) (c1) -> c2
@@ -8,16 +8,92 @@
 -- Πprovincia(σpoblacion=c5 (c4))
 
 
+USE provincias;
+
 -- c1 - Tabla con las autnomías y su población
 SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
   GROUP BY autonomia;
 
--- c2 - Valor mas alto de poblacion de las autonomías
-SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
-  GROUP BY autonomi;
+-- c2 - Valor más alto de población de las autonomías
+SELECT MAX(c1.pobauto) FROM (
+  SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+  GROUP BY autonomia
+  )c1;
 
 -- c3 - Autonimía más poblada
-SELECT c1.autonomia FROM (SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
-  GROUP BY autonomia) AS c1
-  WHERE c1.pobauto = (SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
-  GROUP BY autonomia);
+SELECT c1.autonomia FROM (
+  SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+  GROUP BY autonomia
+  )c1
+  WHERE c1.pobauto = (
+    SELECT MAX(c1.pobauto) FROM (
+    SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+    GROUP BY autonomia
+    )c1
+  );
+
+-- c4 - Tabla con las provincias de la autonomía más poblada y sus valores de población
+SELECT provincia, poblacion FROM provincias
+  WHERE autonomia = (
+    SELECT c1.autonomia FROM (
+    SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+    GROUP BY autonomia
+    )c1
+    WHERE c1.pobauto = (
+      SELECT MAX(c1.pobauto) FROM (
+      SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+      GROUP BY autonomia
+      )c1
+    )
+  );
+
+-- c5 - Valor mínimo de población de la tabla con las provincias de la autonomía más poblada y su población
+SELECT MIN(c4.poblacion) FROM (
+  SELECT provincia, poblacion FROM provincias
+    WHERE autonomia = (
+      SELECT c1.autonomia FROM (
+      SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+      GROUP BY autonomia
+      )c1
+      WHERE c1.pobauto = (
+        SELECT MAX(c1.pobauto) FROM (
+        SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+        GROUP BY autonomia
+        )c1
+      )
+    )
+  )c4;
+
+-- Solución: El nombre de la provincia menos poblada de la autonomía más poblada
+SELECT c4.provincia FROM (
+  SELECT provincia, poblacion FROM provincias
+    WHERE autonomia = (
+      SELECT c1.autonomia FROM (
+      SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+      GROUP BY autonomia
+      )c1
+      WHERE c1.pobauto = (
+        SELECT MAX(c1.pobauto) FROM (
+        SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+        GROUP BY autonomia
+        )c1
+      )
+    )
+  )c4
+  WHERE poblacion = (
+    SELECT MIN(c4.poblacion) FROM (
+    SELECT provincia, poblacion FROM provincias
+      WHERE autonomia = (
+        SELECT c1.autonomia FROM (
+        SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+        GROUP BY autonomia
+        )c1
+        WHERE c1.pobauto = (
+          SELECT MAX(c1.pobauto) FROM (
+          SELECT autonomia, SUM(poblacion) AS pobauto FROM provincias
+          GROUP BY autonomia
+          )c1
+        )
+      )
+    )c4
+  );
