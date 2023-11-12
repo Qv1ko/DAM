b05041ec50ca8693ba28819b34e4ae59746dcd40
Qv1ko <vicgarmur947@gmail.com>
2023-11-12 01:17:48 +0100

    Ejercicio 1 consultas 1 - 7 y 9

---
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio01.sql
@@ -0,0 +1,68 @@
+﻿USE ciclistas; -- σ Π Ģ
+
+-- 1. Listar las edades de los ciclistas (sin repetidos)
+
+-- Πedad (ciclista)
+SELECT DISTINCT edad FROM ciclista;
+
+-- 2. Listar las edades de los ciclistas de Artiach
+
+-- Πedad (σnomequipo='Artiach') (ciclista)
+SELECT DISTINCT edad FROM ciclista
+  WHERE nomequipo='Artiach';
+
+-- 3. Listar las edades de los ciclistas de Artiach o de Amore Vita
+
+-- Πedad (σnomequipo='Artiach' v nomequipo='Amore Vita') (ciclista)
+SELECT DISTINCT edad FROM ciclista
+  WHERE nomequipo='Artiach' OR nomequipo='Amore Vita';
+
+-- 4. Listar los dorsales de los ciclistas cuya edad sea menor que 25 o mayor que 30
+
+-- Πdorsal (σedad<25 v edad>30) (ciclista)
+SELECT DISTINCT dorsal FROM ciclista
+  WHERE edad<25 OR edad>30;
+
+-- 5. Listar los dorsales de los ciclistas cuya edad esté entre 28 y 32 y además que sólo sean de Banesto
+
+-- Πdorsal (σedad>=28 ^ edad<=32 ^ nomequipo='Banesto') (ciclista)
+SELECT DISTINCT dorsal FROM ciclista
+  WHERE edad BETWEEN 28 AND 32 AND nomequipo='Banesto';
+
+-- 6. Indícame el nombre de los ciclistas que el número de caracteres del nombre sea mayor que 8
+
+-- Πnombre (σ(Ģlength(nombre))>8) (ciclista)
+SELECT DISTINCT nombre FROM ciclista
+  WHERE LENGTH(nombre)>8;
+
+-- 7. Lístame el nombre y el dorsal de todos los ciclistas mostrando un campo nuevo denominado nombre mayúsculas que debe mostrar el nombre en mayúsculas
+
+-- Ģnombre,dorsal,upper(nombre) (ciclista)
+SELECT nombre, dorsal, UPPER(nombre) AS 'Nombre mayúsculas' FROM ciclista;
+
+-- 8. Listar todos los ciclistas que han ganado el maillot MGE (amarillo) en alguna etapa
+
+-- Πdorsal (σcódigo='MGE') (lleva) -> c1
+SELECT DISTINCT dorsal FROM lleva
+    WHERE código='MGE';
+-- σdorsal=(c1) (ciclista)
+/*
+SELECT * FROM ciclista
+  WHERE dorsal=(
+    SELECT DISTINCT dorsal FROM lleva
+    WHERE código='MGE'
+  );
+*/
+
+-- (9) Listar el nombre de los puertos cuya altura sea mayor de 1500 
+
+-- Πnompuerto (σaltura>1500) (puerto)
+SELECT DISTINCT nompuerto FROM puerto
+  WHERE altura>1500;
+
+-- (10) Listar el dorsal de los ciclistas que hayan ganado algun puerto cuya pendiente sea mayor que 8 o cuya altura esté entre 1800 y 3000
+
+-- 
+
+
+-- (11) Listar el dorsal de los ciclistas que hayan ganado algún puerto cuya pendiente sea mayor que 8 y cuya altura esté entre 1800 y 3000
