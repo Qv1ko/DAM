fc44daab0cdd754418ae15b83d79a75677bcdac0
Qv1ko <vicgarmur947@gmail.com>
2023-11-12 13:10:44 +0100

    Ejercicio 1 consultas

--- Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio01.sql
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio01.sql
@@ -42,27 +42,18 @@ SELECT nombre, dorsal, UPPER(nombre) AS 'Nombre mayúsculas' FROM ciclista;
 
 -- 8. Listar todos los ciclistas que han ganado el maillot MGE (amarillo) en alguna etapa
 
--- Πdorsal (σcódigo='MGE') (lleva) -> c1
-SELECT DISTINCT dorsal FROM lleva
-    WHERE código='MGE';
--- σdorsal=(c1) (ciclista)
-/*
-SELECT * FROM ciclista
-  WHERE dorsal=(
-    SELECT DISTINCT dorsal FROM lleva
-    WHERE código='MGE'
-  );
-*/
+-- Π(σcódigo='MGE') (ciclista,lleva)
+SELECT DISTINCT c.* FROM ciclista c INNER JOIN lleva l ON c.dorsal = l.dorsal
+    WHERE l.código='MGE';
 
--- (9) Listar el nombre de los puertos cuya altura sea mayor de 1500 
+-- 9. Listar el nombre de los puertos cuya altura sea mayor de 1500 
 
 -- Πnompuerto (σaltura>1500) (puerto)
 SELECT DISTINCT nompuerto FROM puerto
   WHERE altura>1500;
 
--- (10) Listar el dorsal de los ciclistas que hayan ganado algun puerto cuya pendiente sea mayor que 8 o cuya altura esté entre 1800 y 3000
+-- 10. Listar el dorsal de los ciclistas que hayan ganado algun puerto cuya pendiente sea mayor que 8 o cuya altura esté entre 1800 y 3000
 
--- 
-
-
--- (11) Listar el dorsal de los ciclistas que hayan ganado algún puerto cuya pendiente sea mayor que 8 y cuya altura esté entre 1800 y 3000
+-- Πdorsal(σpendiente>8 v altura>=1800 ^ altura<=3000) (ciclista,puerto)
+SELECT DISTINCT c.dorsal FROM ciclista c INNER JOIN puerto p ON c.dorsal = p.dorsal
+  WHERE p.pendiente>8 OR p.altura BETWEEN 1800 AND 3000;
