4a348c3b04b306ec0ae0f576d262be1a959bebe4
Qv1ko <vicgarmur947@gmail.com>
2023-11-12 13:37:51 +0100

    Enunciados ejercicio 2

---
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio02.sql
@@ -0,0 +1,38 @@
+﻿-- 1. Número de ciclistas que hay
+
+
+-- 2. Número de ciclistas que hay del equipo Banesto
+
+
+-- 3. Edad media de los ciclistas
+
+
+-- 4. La edad media de los del equipo Banesto
+
+
+-- 5. La edad media de los ciclistas por cada equipo
+
+
+-- 6. El número de ciclistas por equipo
+
+
+-- 7. El número total de puertos
+
+
+
+-- 8. El número total de puertos mayores de 1500
+
+
+
+-- 9. Listar el nombre de los equipos que tengan más de 4 ciclistas
+
+
+
+-- 10. Listar el nombre de los equipos que tengan más de 4 ciclistas cuya edad esté entre 28 y 32
+
+
+-- 22. Indícame el número de etapas que ha ganado cada uno de los ciclistas
+
+
+-- 12. Indícame el dorsal de los ciclistas que hayan ganado más de una etapa
+
