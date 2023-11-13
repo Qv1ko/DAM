3ce8af881fb7e627a60ef4cad70e9bffc24abc7a
Qv1ko <vicgarmur947@gmail.com>
2023-11-13 21:37:59 +0100

    Enunciados ejercicio 3

---
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio03.sql
@@ -0,0 +1,41 @@
+use ciclistas; -- σ Π Ģ
+
+-- 1. Listar las edades de todos los ciclistas de Banesto
+
+
+
+-- 2. Listar las edades de los ciclistas que son de Banesto o de Navigare
+
+
+
+-- 3. Listar el dorsal de los ciclistas que son de Banesto y cuya edad está entre 25 y 32
+
+
+
+-- 4. Listar el dorsal de los ciclistas que son de Banesto o cuya edad está entre 25 y 32
+
+
+
+-- 5. Listar la inicial del equipo de los ciclistas cuyo nombre comience por R
+
+
+
+-- 6. Listar el código de las etapas que su salida y llegada sea en la misma población
+
+
+
+-- 7. Listar el código de las etapas que su salida y llegada no sean en la misma población y que conozcamos el dorsal del ciclista que ha ganado
+
+
+
+-- 8. Listar el nombre de los puertos cuya altura esté entre 1000 y 2000 o que la altura sea mayor que 2400
+
+
+
+-- 9. Listar el dorsal de los ciclistas que hayan ganado los puertos cuya altura esté entre 1000 y 2000 o que la altura sea mayor que 2400
+
+
+
+-- 10. Listar el número de ciclistas que hayan ganado alguna etapa
+
+
