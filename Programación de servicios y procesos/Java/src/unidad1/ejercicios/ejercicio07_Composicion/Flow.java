a9d8f97999d234a9e7ff3e0adcfa641ec3ffb22b
Qv1ko <vicgarmur947@gmail.com>
2023-11-13 22:31:54 +0100

    Ejercicio composición

--- Programación de servicios y procesos/Java/src/unidad1/ejercicios/ejercicio07_Composicion/Flow.java
+++ Programación de servicios y procesos/Java/src/unidad1/ejercicios/ejercicio07_Composicion/Flow.java
@@ -60,7 +60,91 @@ class Monoplaza {
     }
 
     public void mostrar() {
-        
+        System.out.println("Monoplaza de (" + getLargo() + ", " + getAncho() + ", " + getAlto() + " con una altura de " + getPeso() + " Kg");
     }
+
+}
+
+class Formula1 extends Monoplaza {
+
+    private int potencia, depositoCombustible, cambios;
+    private double aceleracion;
+    private boolean drs;
+
+    public Formula1() {
+        super();
+        this.potencia = 0;
+        this.aceleracion = 0;
+        this.drs = false;
+        this.depositoCombustible = 0;
+        this.cambios = 0;
+    }
+
+    public Formula1(double largo, double ancho, double alto, int peso, int potencia, double aceleracion, boolean drs, int depositoCombustible, int cambios) {
+        super(largo, ancho, alto, peso);
+        this.potencia = potencia;
+        this.aceleracion = aceleracion;
+        this.drs = drs;
+        this.depositoCombustible = depositoCombustible;
+        this.cambios = cambios;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+    
+    public double getAceleracion() {
+        return aceleracion;
+    }
+
+    public boolean getDrs() {
+        return drs;
+    }
+    
+    public int getDepositoCombustible() {
+        return depositoCombustible;
+    }
+
+    public int getCambios() {
+        return cambios;
+    }
+        
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+    
+        public void setAceleracion(double aceleracion) {
+            this.aceleracion = aceleracion;
+        }
+    
+        public void setDrs(boolean drs) {
+            this.drs = drs;
+        }
+
+    public void setDepositoCombustible(int depositoCombustible) {
+        this.depositoCombustible = depositoCombustible;
+    }
+
+    public void setCambios(int cambios) {
+        this.cambios = cambios;
+    }
+
+    public void mostrar() {
+        super.mostrar();
+        System.out.println("Formula 1 con una potencia de " + getPotencia() + " rpm, una aceleracion de 0 a 100 Km/h de " + getAceleracion() + " segundos, " + (getDrs() ? "con DRS " : "sin DRS ") + ", con un deposito de " + getDepositoCombustible() + " litros y una caja de cambios de " + getCambios() + " velocidades");
+    }
+
+    public String toString() {
+        return "Formula 1 con una potencia de " + getPotencia() + " rpm, una aceleracion de 0 a 100 Km/h de " + getAceleracion() + " segundos, " + (getDrs() ? "con DRS " : "sin DRS ") + ", con un deposito de " + getDepositoCombustible() + " litros y una caja de cambios de " + getCambios() + " velocidades";
+    }
+
+}
+
+class FormulaE {
+
+    private int potenciaElectrica, velocidad, regeneracion;
+    private String trenMotriz;
+
+    
 
 }
