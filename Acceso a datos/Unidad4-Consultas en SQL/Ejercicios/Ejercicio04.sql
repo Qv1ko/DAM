6c7e52740eb212a642bd5c343598a33a0e67c805
Victor Garcia Murillo <vgarciam@ceinmark.net>
2023-11-14 21:26:01 +0100

    Enunciados ejercicio 4 de consultas

---
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio04.sql
@@ -0,0 +1,37 @@
+USE ciclistas; -- σ Π Ģ
+
+-- 1. Listar el número de etapas que tengan puerto
+
+
+
+-- 2. Listar el número de ciclistas que hayan ganado algún puerto
+
+
+
+-- 3. Listar el código de la etapa con el número de puertos que tiene
+
+
+
+-- 4. Indicar la altura media de los puertos
+
+
+
+-- 5. Indicar el código de etapa cuya altura media de sus puertos está por encima de 1500
+
+
+
+-- 6. Indicar el número de etapas que cumplen la condición anterior
+
+
+
+-- 7. Listar el dorsal del ciclista con el número de veces que ha llevado algún maillot
+
+
+
+-- 8. Listar el dorsal del ciclista con el código de maillot y cuántas veces ese ciclista ha llevado ese maillot
+
+
+
+-- 9. Listar el dorsal, el código de etapa, el ciclista y el número de maillots que ese ciclista ha llevado en cada etapa
+
+
