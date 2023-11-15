1a76c77d879979de368b71b6f20863a7d92eed78
Víctor García <vicgarmur947@gmail.com>
2023-11-15 20:44:02 +0100

    Ejercicio 4 de consultas

--- Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio04.sql
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio04.sql
@@ -31,20 +31,37 @@ SELECT AVG(altura) FROM puerto;
 
 -- 5. Indicar el código de etapa cuya altura media de sus puertos está por encima de 1500
 
--- 
+-- numetapaĢnumetapa,avg(altura) (puerto) -> c1
+SELECT numetapa, AVG(altura) alturamedia FROM puerto
+  GROUP BY numetapa;
+-- Πnumetapa (σalturamedia>1500 (puerto))
+SELECT DISTINCT c1.numetapa FROM (
+    SELECT numetapa, AVG(altura) alturamedia FROM puerto
+    GROUP BY numetapa
+  )c1
+  WHERE c1.alturamedia>1500;
 
 -- 6. Indicar el número de etapas que cumplen la condición anterior
 
-
+-- Ģcount(numetapa) (σalturamedia>1500 (puerto))
+SELECT COUNT(c1.numetapa) FROM (
+    SELECT numetapa, AVG(altura) alturamedia FROM puerto
+    GROUP BY numetapa
+  )c1
+  WHERE c1.alturamedia>1500;
 
 -- 7. Listar el dorsal del ciclista con el número de veces que ha llevado algún maillot
 
-
+-- dorsalĢdorsal,count(*) (lleva)
+SELECT dorsal, COUNT(*) FROM lleva
+  GROUP BY dorsal;
 
 -- 8. Listar el dorsal del ciclista con el código de maillot y cuántas veces ese ciclista ha llevado ese maillot
 
-
+-- dorsal,códigoĢdorsal,código,count(*) (lleva)
+SELECT dorsal, código, COUNT(*) FROM lleva
+  GROUP BY dorsal, código;
 
 -- 9. Listar el dorsal, el código de etapa, el ciclista y el número de maillots que ese ciclista ha llevado en cada etapa
 
-
+-- 
