ab1cf9be97b839f28528d2b28e4c16d23a9a937b
Víctor García <vicgarmur947@gmail.com>
2023-11-15 13:50:25 +0100

    Ejercicio 4 de consultas

--- Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio04.sql
+++ Acceso a datos/Unidad4-Consultas en SQL/Ejercicios/Ejercicio04.sql
@@ -1,24 +1,37 @@
-USE ciclistas; -- σ Π Ģ
+﻿USE ciclistas; -- σ Π Ģ
 
 -- 1. Listar el número de etapas que tengan puerto
 
-
+-- Πnumetapa (puerto) -> c1
+SELECT DISTINCT numetapa FROM puerto;
+-- Ģcount(*) (c1)
+SELECT COUNT(*) FROM (
+    SELECT DISTINCT numetapa FROM puerto
+  )c1;
 
 -- 2. Listar el número de ciclistas que hayan ganado algún puerto
 
-
+-- Πdorsal (puerto) -> c1
+SELECT DISTINCT dorsal FROM puerto;
+-- Ģcount(*) (c1)
+SELECT COUNT(*) FROM (
+    SELECT DISTINCT dorsal FROM puerto
+  )c1;
 
 -- 3. Listar el código de la etapa con el número de puertos que tiene
 
-
+-- numetapaĢcount(*) (puerto)
+SELECT numetapa, COUNT(*) FROM puerto
+  GROUP BY numetapa;
 
 -- 4. Indicar la altura media de los puertos
 
-
+-- Ģavg(altura) (puerto)
+SELECT AVG(altura) FROM puerto;
 
 -- 5. Indicar el código de etapa cuya altura media de sus puertos está por encima de 1500
 
-
+-- 
 
 -- 6. Indicar el número de etapas que cumplen la condición anterior
 
