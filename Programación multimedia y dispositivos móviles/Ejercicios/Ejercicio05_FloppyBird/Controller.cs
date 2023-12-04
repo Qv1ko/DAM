67d9d51792f3048f85effb03d663ecf6c9c2421f
Víctor García <vicgarmur947@gmail.com>
2023-12-04 22:22:49 +0100

    Añadido botón de salir

---
+++ Programación multimedia y dispositivos móviles/Ejercicios/Ejercicio05_FloppyBird/Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public void Salir() {
+        Application.Quit();
+    }
+
+}
