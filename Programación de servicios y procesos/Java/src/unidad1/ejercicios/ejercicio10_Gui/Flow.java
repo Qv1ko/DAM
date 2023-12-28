1356444e37f3e525fe00511a61c6f000b7c03c72
Víctor García <vicgarmur947@gmail.com>
2023-12-28 02:28:25 +0100

    GUI Euromillos

--- Programación de servicios y procesos/Java/src/unidad1/ejercicios/ejercicio10_Gui/Flow.java
+++ Programación de servicios y procesos/Java/src/unidad1/ejercicios/ejercicio10_Gui/Flow.java
@@ -1,7 +1,11 @@
-package unidad1.ejercicios.ejercicio10_GuiBasico;
+package unidad1.ejercicios.ejercicio10_Gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -25,11 +29,10 @@ class App {
     private JLabel title;
     private JButton greenButton;
     private JLabel text;
-    private JTextField[] numbers = new JTextField[5];
-    private JTextField[] stars = new JTextField[2];
-    private JButton blueButton;
-    private JButton redButton;
+    private JTextField[] numbers = new JTextField[5], stars = new JTextField[2];
+    private JButton redButton, blueButton;
     private JTextArea info;
+    private Set<Integer> generated = new HashSet<Integer>();
 
     public App() {
 
@@ -56,6 +59,7 @@ class App {
         this.greenButton.setForeground(Color.GREEN);
         this.greenButton.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 
+        this.greenButton.addActionListener(actionGreenButton);
         this.frame.getContentPane().add(greenButton);
 
         // First Text
@@ -68,36 +72,36 @@ class App {
         // Winning numbers
         this.numbers[0] = new JTextField();
         this.numbers[0].setBounds(104, 120, 24, 24);
-        this.numbers[0].setColumns(2);
         this.numbers[0].setHorizontalAlignment(JTextField.CENTER);
+        this.numbers[0].setColumns(2);
 
         this.frame.getContentPane().add(this.numbers[0]);
 
         this.numbers[1] = new JTextField();
         this.numbers[1].setBounds(144, 120, 24, 24);
-        this.numbers[1].setColumns(2);
         this.numbers[1].setHorizontalAlignment(JTextField.CENTER);
+        this.numbers[1].setColumns(2);
 
         this.frame.getContentPane().add(this.numbers[1]);
 
         this.numbers[2] = new JTextField();
         this.numbers[2].setBounds(184, 120, 24, 24);
-        this.numbers[2].setColumns(2);
         this.numbers[2].setHorizontalAlignment(JTextField.CENTER);
+        this.numbers[2].setColumns(2);
 
         this.frame.getContentPane().add(this.numbers[2]);
 
         this.numbers[3] = new JTextField();
         this.numbers[3].setBounds(224, 120, 24, 24);
-        this.numbers[3].setColumns(2);
         this.numbers[3].setHorizontalAlignment(JTextField.CENTER);
+        this.numbers[3].setColumns(2);
 
         this.frame.getContentPane().add(this.numbers[3]);
 
         this.numbers[4] = new JTextField();
         this.numbers[4].setBounds(264, 120, 24, 24);
-        this.numbers[4].setColumns(2);
         this.numbers[4].setHorizontalAlignment(JTextField.CENTER);
+        this.numbers[4].setColumns(2);
 
         this.frame.getContentPane().add(this.numbers[4]);
 
@@ -111,39 +115,41 @@ class App {
         // Winning stars
         this.stars[0] = new JTextField();
         this.stars[0].setBounds(104, 160, 24, 24);
-        this.stars[0].setColumns(2);
         this.stars[0].setHorizontalAlignment(JTextField.CENTER);
+        this.stars[0].setColumns(2);
 
         this.frame.getContentPane().add(this.stars[0]);
 
         this.stars[1] = new JTextField();
         this.stars[1].setBounds(144, 160, 24, 24);
-        this.stars[1].setColumns(2);
         this.stars[1].setHorizontalAlignment(JTextField.CENTER);
+        this.stars[1].setColumns(2);
 
         this.frame.getContentPane().add(this.stars[1]);
 
-        // Blue Button
-        this.blueButton = new JButton("ENVIAR");
-        this.blueButton.setBounds(96, 200, 96, 24);
-        this.blueButton.setFont(new Font("Arial", Font.BOLD, 16));
-
-        this.blueButton.setBackground(Color.WHITE);
-        this.blueButton.setForeground(Color.BLUE);
-        this.blueButton.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-
-        this.frame.getContentPane().add(blueButton);
-
         // Red Button
-        this.redButton = new JButton("BORRAR");
-        this.redButton.setBounds(208, 200, 96, 24);
+        this.redButton = new JButton("ENVIAR");
+        this.redButton.setBounds(96, 200, 96, 24);
         this.redButton.setFont(new Font("Arial", Font.BOLD, 16));
 
         this.redButton.setBackground(Color.WHITE);
         this.redButton.setForeground(Color.RED);
         this.redButton.setBorder(BorderFactory.createLineBorder(Color.RED));
 
+        this.redButton.addActionListener(actionRedButton);
         this.frame.getContentPane().add(redButton);
+
+        // Blue Button
+        this.blueButton = new JButton("BORRAR");
+        this.blueButton.setBounds(208, 200, 96, 24);
+        this.blueButton.setFont(new Font("Arial", Font.BOLD, 16));
+
+        this.blueButton.setBackground(Color.WHITE);
+        this.blueButton.setForeground(Color.BLUE);
+        this.blueButton.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+
+        this.blueButton.addActionListener(actionBlueButton);
+        this.frame.getContentPane().add(blueButton);
 
         // Third Text
         this.text = new JLabel("Información");
@@ -155,8 +161,8 @@ class App {
         // Information Area
         this.info = new JTextArea();
         this.info.setBounds(16, 280, 352, 64);
-        this.info.setFont(new Font("Arial", Font.BOLD, 12));
         this.info.setBorder(new EmptyBorder(8, 8, 8, 8));
+        this.info.setFont(new Font("Arial", Font.BOLD, 12));
 
         this.frame.getContentPane().add(this.info);
 
@@ -164,5 +170,54 @@ class App {
         frame.setVisible(true);
 
     }
+
+    ActionListener actionGreenButton = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            generated.clear();
+            for (JTextField number : numbers) {
+                number.setText(randomNumber(50));
+            }
+
+            generated.clear();
+            for (JTextField star : stars) {
+                star.setText(randomNumber(12));
+            }
+
+        }
+
+    };
+
+    private String randomNumber(int max) {
+
+        int result;
+
+        do {
+            result = (int) (Math.random() * max) + 1;
+        } while (!generated.add(result));
+
+        return Integer.toString(result);
+
+    }
+
+    ActionListener actionRedButton = new ActionListener() {
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            info.setText("Sorteo de Euromillones\nNúmeros: " + numbers[0].getText() + "-" + numbers[1].getText() + "-" + numbers[2].getText() + "-" + numbers[3].getText() + "-" + numbers[4].getText() + "\nEstrellas: " + stars[0].getText() + "-" + stars[1].getText());
+        }
+        
+    };
+    
+    ActionListener actionBlueButton = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            info.setText("");
+        }
+        
+    };
 
 }
