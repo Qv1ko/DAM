9bf2201c4d206bad5f82327a9eacd1931ff4d814
Víctor García <vicgarmur947@gmail.com>
2024-02-01 10:50:44 +0100

    Ejemplo de proceso con ProcessBuilder

---
+++ Programación de servicios y procesos/Java/src/unidad2/clases/Clase01_ProcessBuilder.java
@@ -0,0 +1,35 @@
+package unidad2.clases;
+
+import java.io.IOException;
+
+public class Clase01_ProcessBuilder {
+    
+    public static void main(String[] args) {
+
+        String[] a = new String[2];
+        
+        a[0] = "notepad.exe";
+        a[1] = "C:\\Users\\Usuario\\Desktop\\ejemplo.txt";
+    
+        Process p;
+        ProcessBuilder pb = new ProcessBuilder(a);
+    
+        try {
+    
+            p = pb.start();
+    
+            try {
+                p.waitFor();
+            } catch (InterruptedException e) {
+                System.out.println("Error al interrumpir el proceso");
+            }
+    
+        } catch (IOException e) {
+            System.out.println("Error al lanzar el proceso");
+        } finally {
+            System.out.println("Fin del proceso");
+        }
+
+    }
+
+}
